b3f096f9516f0480884886c13fafff6cc1ada02a
abast <arco.bast@yahoo.de>
2017-08-18 18:02:51 +0200

    removing deprecated initialization methods

--- test_model_data_base/mdb_initializers/load_simrun_general_test.py
+++ test_model_data_base/mdb_initializers/load_simrun_general_test.py
@@ -1,6 +1,6 @@
 from ..context import *
 from .. import decorators
-from model_data_base.mdb_initializers.load_roberts_simulationdata import *
+from model_data_base.mdb_initializers.load_simrun_general import *
 import unittest
 import os, shutil
 from mock import MagicMock
@@ -12,18 +12,17 @@ class Tests(unittest.TestCase):
     def setUp(self):
         self.path = tempfile.mkdtemp()
         self.mdb = model_data_base.ModelDataBase(self.path)
-        self.mdb.settings.show_computation_progress = False
+        self.mdb.settings.show_computation_progress = False        
     def tearDown(self):
         if os.path.exists(self.path):
             shutil.rmtree(self.path)
 
-    @decorators.testlevel(2)                
+    @decorators.testlevel(2)        
     def test_can_build_database(self):
         mdb = self.mdb
         init(mdb, test_data_folder)
-        pipeline(mdb)
+        optimize(mdb)
         assert(isinstance(mdb['spike_times'].iloc[0][0], float))
         assert(isinstance(mdb['voltage_traces'].head().iloc[0][0], float))
         self.assertIsInstance(mdb['voltage_traces'].columns[0], float)
-        self.assertIsInstance(mdb['voltage_traces'].head().columns[0], float)        
-        
+        self.assertIsInstance(mdb['voltage_traces'].head().columns[0], float)
