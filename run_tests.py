c8336353213e4a36518cb280621db6f53c8e3bb4
abast <arco.bast@yahoo.de>
2017-08-19 13:26:01 +0200

    minor improvement

--- run_tests.py
+++ run_tests.py
@@ -1,84 +1,47 @@
-import os, shutil
 import unittest
-import test_model_data_base, test_model_data_base.decorators, test_model_data_base.context
-#import test_simrun2, test_simrun2.decorators
-import model_data_base
-import simrun2
-
-
-#####################################################################
-# make sure everything is in defined state before running tests.
-##########################################################
-# todo: maybe put these setup routines somewhere else?
-
-# set up mdb
-test_mdb_folder = test_model_data_base.context.test_mdb_folder
-if os.path.exists(test_mdb_folder):
-    shutil.rmtree(test_mdb_folder)
-mdb = model_data_base.ModelDataBase(test_mdb_folder)
-mdb.settings.show_computation_progress = False
-model_data_base.mdb_initializers.load_simrun_general.init(mdb, \
-                                  test_model_data_base.context.test_data_folder) 
-
-# setup folder for output files by test suite
-files_generated_by_tests =  test_model_data_base.context.files_generated_by_tests
-if os.path.exists(files_generated_by_tests):
-    shutil.rmtree(files_generated_by_tests)
-os.makedirs(files_generated_by_tests)
-
-
-########################################################
-# end of setup
-########################################################
-
 
 # switch matplotlib backend to make sure that test suite can 
 # runs on machines that do not have graphic libraries installed (Qt, ...)
-
 import matplotlib
 import matplotlib.pyplot as plt
 plt.switch_backend('agg') 
 
- 
+# import modules, whose tests should run 
+import test_model_data_base, test_model_data_base.decorators
+import test_simrun2, test_simrun2.decorators
+
+#######################################
+# SET TESTLEVEL HERE
+# Choose a low testlevel if you only want to run quick tests.
+#
+# The testlevel of tests can be set using a decorator defined in:
+# @decorators.testlevel(some_number)
+#
+# Valid values are integers and 'all'
+####################################### 
+test_model_data_base.decorators.current_testlevel = 100#'all'#0#'all'#'0
+test_simrun2.decorators.current_testlevel = 100#'all'#0#'all'#'0
+
+#######################################
+# SELECT TESTS YOU WANT TO RUN HERE
+#
+# Valid values are strings, which contain a module name, that can be 
+# imported. Choose '.' to run all available tests within this module
+#######################################
+run = '.'
+#run = 'test_model_data_base.IO.roberts_formats_test'
+################################
+# verbosity of testrunner
+################################
+verbosity = 2
+    
+    
 if __name__ == "__main__":
-
-    ##################################################
-    # configure test runs here
-    ##################################################
-    
-    #if testlevel is set to integer, tests with the decorator
-    #unittest.monkypatches.run_if_testlevel(level) are only run
-    #if testlevel >= level.
-    #if testlevel is set to 'all', all tests will run.
-    test_model_data_base.decorators.current_testlevel = 100#'all'#0#'all'#'0
-#    test_simrun2.decorators.current_testlevel = 1#'all'#0#'all'#'0
-    
-    
-#     unittest.monkypatches.testlevel = 'all'
-    
-    #if run is set to a string resembling the path of a module
-    #containing unittest.TestCase object, only the tests in the
-    #speified module will run.
-    #if run is set to 'all', all tests in the test module will run.
-    run = 'test_model_data_base.mdb_initializers.load_crossing_over_results_test'
-
- #   run = 'all'
-    #run = 'test.plotfunctions.manylines_test'
-    
-    #verbosity of testrunner
-    verbosity = 5
-    #######################################################
-    # end of configuration
-    ########################################################
-
-    
     testRunner = unittest.TextTestRunner(verbosity = verbosity)
-    
-    if run == 'all': #testdiscovery
-        tests = unittest.defaultTestLoader.discover('.', pattern = '*_test.py')
-        testRunner.run(tests)   
-    
-    else: #run tests in specified module
-        test = __import__(run, globals(), locals(), [], 0)
-        suite = eval('unittest.TestLoader().loadTestsFromTestCase(%s.Tests)' % run)        
-        testRunner.run(suite)
+    tests = unittest.defaultTestLoader.discover(run, pattern = '*_test.py')
+    testRunner.run(tests)          
+#     
+#     else: #run tests in specified module
+#         test = __import__(run, globals(), locals(), [], 0)
+#         suite = eval('unittest.TestLoader().loadTestsFromTestCase(%s.Tests)' % run)        
+#         testRunner.run(suite)
