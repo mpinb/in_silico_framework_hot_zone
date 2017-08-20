10f513546a25c22f3e0394ca8662b322e4356f82
abast <arco.bast@yahoo.de>
2017-08-21 00:27:15 +0200

    restructuring: all data is in getting_started

--- test_model_data_base/context.py
+++ test_model_data_base/context.py
@@ -4,13 +4,18 @@ parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.insert(0, parent)
 
 from model_data_base import *
+import getting_started
 
 # set up paths
-cell_param_path = os.path.join(parent, 'test_model_data_base/data/test_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/20150815-1530_20240/20240_neuron_model.param')
-assert(os.path.exists(cell_param_path))
-network_param_path = os.path.join(parent, 'test_model_data_base/data/test_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/20150815-1530_20240/20240_network_model.param')
-assert(os.path.exists(network_param_path))
-test_data_folder = os.path.join(parent, 'test_model_data_base/data/test_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/')
+test_data_folder = os.path.join(getting_started.parent, \
+                              'example_simulation_data', \
+                              'C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/')
+
+# cell_param_path = os.path.join(parent, 'test_model_data_base/data/test_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/20150815-1530_20240/20240_neuron_model.param')
+# assert(os.path.exists(cell_param_path))
+# network_param_path = os.path.join(parent, 'test_model_data_base/data/test_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/20150815-1530_20240/20240_network_model.param')
+# assert(os.path.exists(network_param_path))
+# test_data_folder = os.path.join(parent, 'test_model_data_base/data/test_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/')
 assert(os.path.exists(test_data_folder))
 files_generated_by_tests = os.path.join(parent, 'test_model_data_base/data/files_generated_by_tests')
 if os.path.exists(files_generated_by_tests):
