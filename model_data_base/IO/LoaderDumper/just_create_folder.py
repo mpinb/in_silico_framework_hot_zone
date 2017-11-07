d197dad6bc8435d05bca5b8f990684d4f89603f8
abast <arco.bast@yahoo.de>
2017-11-07 22:03:13 +0100

    loading in a subdirectory in the database, which was previously created using create_managed_folder returns a ManagedFolder object. This object is inherited from str, but supports additional functionality

--- model_data_base/IO/LoaderDumper/just_create_folder.py
+++ model_data_base/IO/LoaderDumper/just_create_folder.py
@@ -6,9 +6,21 @@ def check(obj):
     '''checks wherther obj can be saved with this dumper'''
     return obj is None #isinstance(obj, np) #basically everything can be saved with pickle
 
+class ManagedFolder(str):
+    def __new__(cls, s, mdb):
+        obj = str.__new__(cls, s)
+        obj.mdb = mdb
+        return obj
+    def __init__(self, s, mdb):
+        str.__init__(s)
+        self.mdb = mdb
+    def join(self, *args):
+        return ManagedFolder(os.path.join(self, *args), self.mdb)
+
 class Loader(parent_classes.Loader):
     def get(self, savedir):
-        return savedir
+        #return savedir
+        return ManagedFolder(savedir, None)
     
 def dump(obj, savedir):
     with open(os.path.join(savedir, 'Loader.pickle'), 'w') as file_:
