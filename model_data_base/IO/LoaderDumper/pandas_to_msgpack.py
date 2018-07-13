0580abb80d1685ddafe73d09adb433b14f591881
Arco Bast <abast@golgi.ad01.caesar.de>
2018-07-13 13:12:17 -0400

    activate blosc compression

--- model_data_base/IO/LoaderDumper/pandas_to_msgpack.py
+++ model_data_base/IO/LoaderDumper/pandas_to_msgpack.py
@@ -9,10 +9,10 @@ def check(obj):
 
 class Loader(parent_classes.Loader):
     def get(self, savedir):
-        return pd.read_msgpack(os.path.join(savedir, 'pandas_to_msgpack.pickle'))
+        return pd.read_msgpack(os.path.join(savedir, 'pandas_to_msgpack'))
     
 def dump(obj, savedir):
-    obj.to_msgpack(os.path.join(savedir, 'pandas_to_msgpack.pickle'))
+    obj.to_msgpack(os.path.join(savedir, 'pandas_to_msgpack'), compress = 'blosc')
 
     with open(os.path.join(savedir, 'Loader.pickle'), 'w') as file_:
         cloudpickle.dump(Loader(), file_)
