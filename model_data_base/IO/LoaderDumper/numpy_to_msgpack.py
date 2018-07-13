0239d5fdf7a5a75cd896999b0d0b5f776d84897e
Arco Bast <abast@golgi.ad01.caesar.de>
2018-07-13 13:13:29 -0400

    add dumpers for numpy (npz format and msgpack format)

---
+++ model_data_base/IO/LoaderDumper/numpy_to_msgpack.py
@@ -0,0 +1,21 @@
+import os
+import cloudpickle
+import pandas as pd
+import numpy as np
+import parent_classes
+
+def check(obj):
+    '''checks wherther obj can be saved with this dumper'''
+    return isinstance(obj, np) #basically everything can be saved with pickle
+
+class Loader(parent_classes.Loader):
+    def get(self, savedir):
+        return pd.read_msgpack(os.path.join(savedir, 'pandas_to_msgpack')).values
+    
+def dump(obj, savedir):
+    obj = pd.DataFrame(obj)
+    obj.to_msgpack(os.path.join(savedir, 'pandas_to_msgpack'), compress = 'blosc')
+    with open(os.path.join(savedir, 'Loader.pickle'), 'w') as file_:
+        cloudpickle.dump(Loader(), file_)
+    
+
