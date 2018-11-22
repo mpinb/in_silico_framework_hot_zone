2a7ecf318f8b1754bfc8f798eb1e171b066e6182
Amir Najafgholi <amir.najafgholi@gmail.com>
2018-11-22 20:24:17 +0100

    adding parent's directory path to the system path in case it is not exist, so we are able to import our own modules

--- radii/radii/__init__.py
+++ radii/radii/__init__.py
@@ -1,5 +1,5 @@
 # See https://docs.python-guide.org/writing/logging/#logging-in-a-library
-import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+#import logging
+#logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-from . import getRad
+from . import getrad
