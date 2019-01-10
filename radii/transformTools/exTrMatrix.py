a2cbb3c9cdad1927b6669fbe5a0b71e6e785ac29
Amir Najafgholi <amir.najafgholi@gmail.com>
2019-01-10 21:39:43 +0100

    read transformation matrixes from files and reuturn a 4*4 matrix

--- radii/transformTools/exTrMatrix.py
+++ radii/transformTools/exTrMatrix.py
@@ -1,7 +1,22 @@
 import numpy as np
+import re
 
 
-# def prepareMatrice(pointSet):
-    
+def read(file):
+    matrix = []
+    vector = []
+    row = []
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        for idx, line in enumerate(lines):
+            if line.rfind("TransformationMatrix")>-1:
+                matches = re.findall('-?\d+\.\d+|-?\d+', line)
+                vector = map(float, matches)
+        for i in range(4):
+            for j in range(4):
+                k = j + i*4
+                row.append(vector[k])
+            matrix.append(row)
+            row = []
 
- 
+    return matrix
