2da14bcb3fe24cf7567e4ee3af51f64d86ad7958
Amir Najafgholi <amir.najafgholi@gmail.com>
2019-02-07 20:44:32 +0100

    add doc string to main python files

--- radii/notebooks/getAllRadSets.py
+++ radii/notebooks/getAllRadSets.py
@@ -1,9 +1,6 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
+# By running the below code we can extract the radii from a
+# bunch of am files which they are in a folder. by providing their folder path
+# and their corresponding tif image folder path
 import os
 import sys
 
@@ -11,10 +8,7 @@ nb_dir = os.path.split(os.getcwd())[0]
 if (nb_dir not in sys.path):
     sys.path.append(nb_dir)
 
-
-# In[2]:
-
-
+# Set folder paths
 amDataPath = str('../data/report/input/am/')
 tifDataPath = str('../data/report/input/tif/max_z_projections/')
 amOutputPath = str('../data/report/output/am/')
@@ -22,15 +16,10 @@ outputFolderPath = str('../data/report/output/am/')
 tifOutputPath = str('../data/report/output/tif/')
 
 
-# In[3]:
-
-
 import radii as radi
 getRad = radi.exRadSets.exRadSets
 
-
-# In[4]:
-
-
+# extract all radii from all am files in folder path and writing them
+# in output files in outputFolderPath
 getRad(amDataPath, tifDataPath, outputFolderPath)
 
