347c00e25774a7f274901d4088b0b7f270edbefb
Arco Bast <arco.bast@yahoo.de>
2019-03-10 20:09:16 +0100

    extend documentation

--- biophysics_fitting/combiner.py
+++ biophysics_fitting/combiner.py
@@ -15,10 +15,42 @@ class Combiner_Setup:
         self.names.append(name)
         
 class Combiner:
+    '''This  class can be used to combine features (usually) computed by an Evaluator object.
+    
+    For a Simulator object s and a Evaluator object e and a Combiner object c, the typical usecase is:
+    voltage_traces_dict = s.run(params)
+    features = e.evaluate(voltage_traces_dict)
+    combined_features = c.combine(features)
+            
+    Internally, the Combiner iterates over all names of specified combinations. Each combination
+    is specified not only by a name of the combination, but also a list of names of the features,
+    that go into that combination. Each list of features is then combined by calling combinefun with that list.
+    
+    Example: Assume, the evaluator returns a dictionary like:
+    features = {'feature1': 1, 'feature2': 2, 'feature3': 3, 'feature4': 4}
+    We want to combine feature1 and 2. We also want to combine features2, 3 and 4. Combining features should be done 
+    by taking the maximum.
+    
+    How can this be set up?
+    An example, how the Combiner object can be set up can be found in hay_complete_default_setup.    
+    c = Combiner()
+    c.setup.append('combination1', ['feature1', 'feature2'])
+    c.setup.append('combination2', ['feature2', 'feature3', 'feature4'])
+    c.setup.combinefun = max
+    
+    combined_features = c.combine(features)
+    combined_features
+    > {'combination1': 2, 'combination2': 4}
+    '''    
     def __init__(self):
         self.setup = Combiner_Setup()
         
     def combine(self, features):
+        '''Combines features that are computed by an Evaluator class.
+        
+        Details, how to set up the Combiner are in the docstring of
+        the Combiner class.   
+        '''
         out = {}
         for n, c in zip(self.setup.names, self.setup.combinations):
             l = [features[k] for k in c]
--- biophysics_fitting/evaluator.py
+++ biophysics_fitting/evaluator.py
@@ -11,10 +11,56 @@ class Evaluator_Setup:
 
 class Evaluator:
     def __init__(self):
+        '''This  class can be used to extract features from (usually) voltagetraces
+        of different stimuli. The voltage traces are (usually) computed with a Simulator 
+        object by calling its run method.
+        
+        In an optimization the features returned by Evaluator.evaluate are saved together
+        with to corresponding parameter values.
+
+        Note: Combining features to reduce the number of objectives should be done with the Combiner object.        
+        
+        For a Simulator object s and a Evaluator object e, the typical usecase is:
+        voltage_traces_dict = s.run(params)
+        features = e.evaluate(voltage_traces_dict)
+                
+        The "program flow" in the Evaluator can be split in two parts:
+        (1) for each key in the voltage_traces_dict: 
+            ---> apply evaluate_fun, that is registered with a name that matches the key.
+                    Extracts features from the voltage trace.
+        (2) perform arbitrary operations on the resulting dictionary 
+                (e.g. merge it from a nested to a flat dictionary or compute more complex features
+                by combineing features from different stimuli)
+                
+        
+        How can the Evaluator be set up?
+        An example, how the Evaluator object can be set up can be found in hay_complete_default_setup.
+        
+        e = Evaluator()
+        e.setup.evaluate_funs.append([in_name, evaluate_fun, out_name]) # corresponds to step (1)
+        e.setup.finalize_funs.append(finalize_fun) # corresponds to step (2)
+        
+        What form do the functions need to have?
+        def examplary_evaluate_fun(**kwargs):
+            # kwargs are keys and values of voltage_traces_dict[in_name]
+            # extract features from kwargs
+            return out_dict # keys are names of features, values are features
+
+        def finalize_fun(dict_):
+            # dict_ is a dictionary with out_name of the evaluate_funs as keys.
+            # and the returned dictionary out_dict as values, 
+            # i.e. out_dict is a nested dictionary.
+            # dict_ can now be arbitrarily modified, e.g. flattened.
+            return modified_dict
+        '''
         #self.objectives = objectives
         self.setup = Evaluator_Setup()
         
     def evaluate(self, features_dict):
+        '''Extracts features from a simulation result computed by Simulator.run()
+        
+        Details, how to set up the Evaluator are in the docstring of
+        the Evaluator class.'''        
         ret = {}
         for in_name, fun, out_name in self.setup.evaluate_funs:
             ret[out_name] = fun(**features_dict[in_name])
