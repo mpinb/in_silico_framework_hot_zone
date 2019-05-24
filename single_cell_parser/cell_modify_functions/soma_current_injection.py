a815ecfde3b1277c9c1c18f13a09581d5f91d46c
Arco Bast <arco.bast@yahoo.de>
2019-05-24 15:59:46 +0200

    extends neuron_parma format such that current_injections can be defined in the neuron_param file

---
+++ single_cell_parser/cell_modify_functions/soma_current_injection.py
@@ -0,0 +1,7 @@
+from biophysics_fitting.setup_stim import setup_soma_step
+def soma_current_injection(cell, amplitude = None, delay = None, duration = None):
+    setup_soma_step(cell, 
+                    amplitude = amplitude, 
+                    delay = delay, 
+                    duration = duration)
+    return cell
