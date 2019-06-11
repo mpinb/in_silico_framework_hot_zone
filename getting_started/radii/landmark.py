0c4026c7619aa6600a5454f7427bae799501c55f
Amir Najafgholi <amir.najafgholi@gmail.com>
2019-06-11 11:03:02 +0200

    landmark produces am and eg am and eg hoc

--- getting_started/radii/landmark.py
+++ getting_started/radii/landmark.py
@@ -8,24 +8,52 @@ import re
 
 
 # inputFile = '/home/amir/Projects/radii/radii/data/neuron1/landmark/conFile.txt'
-# inputFile = '/home/amir/Projects/radii/radii/data/neuron1/landmark/egAm.txt'
-inputFile = '/home/amir/Projects/radii/radii/data/neuron1/landmark/amTransformed2.txt'
 # outputFile = '/home/amir/Projects/radii/radii/data/neuron1/landmark/hocFile'
 # outputFile = '/home/amir/Projects/radii/radii/data/neuron1/landmark/hocFile_reduced'
 # outputFile = '/home/amir/Projects/radii/radii/data/neuron1/landmark/conFile_transformed'
-# outputFile = '/home/amir/Projects/radii/radii/data/neuron1/landmark/egAm_landmark'
-outputFile = '/home/amir/Projects/radii/radii/data/neuron1/landmark/am2_transformed_landmark'
 
-points = []
+# inputFile = '/home/amir/Projects/in_silico_framework/getting_started/radii/data/neuron2/test/pairs.txt'
+# outputFile = '/home/amir/Projects/in_silico_framework/getting_started/radii/data/neuron2/test/landmark/pairs_landmark'
+
+inputEgHoc = '/home/amir/Projects/in_silico_framework/getting_started/radii/data/neuron2/egHoc.txt'
+outputEgHoc = '/home/amir/Projects/in_silico_framework/getting_started/radii/data/neuron2/landmark/egHoc_landmark'
+
+inputEgAm = '/home/amir/Projects/in_silico_framework/getting_started/radii/data/neuron2/egAm.txt'
+outputEgAm = '/home/amir/Projects/in_silico_framework/getting_started/radii/data/neuron2/landmark/egAm_landmark'
+
+inputAmTr = '/home/amir/Projects/in_silico_framework/getting_started/radii/data/neuron2/amTransformed2.txt'
+outputAmTr = '/home/amir/Projects/in_silico_framework/getting_started/radii/data/neuron2/landmark/am2_transformed_landmark'
+
 
 # opening and reading txt file and set their points to a list of
 # arrays of points
-with open(inputFile, 'r') as hocFile:
-    lines = hocFile.readlines()
+points = []
+with open(inputEgHoc, 'r') as hocLand:
+    lines = hocLand.readlines()
     for line in lines:
         matches = re.findall('-?\d+\.\d+', line)
         point = map(float, matches)
         points.append(point)
+I.scp.write_landmark_file(outputEgHoc, points)
 
-# Writing landmark file
-I.scp.write_landmark_file(outputFile, points)
+
+
+
+points = []
+with open(inputEgAm, 'r') as amLand:
+    lines = amLand.readlines()
+    for line in lines:
+        matches = re.findall('-?\d+\.\d+', line)
+        point = map(float, matches)
+        points.append(point)
+I.scp.write_landmark_file(outputEgAm, points)
+
+
+points = []
+with open(inputAmTr, 'r') as trLand:
+    lines = trLand.readlines()
+    for line in lines:
+        matches = re.findall('-?\d+\.\d+', line)
+        point = map(float, matches)
+        points.append(point)
+I.scp.write_landmark_file(outputAmTr, points)
