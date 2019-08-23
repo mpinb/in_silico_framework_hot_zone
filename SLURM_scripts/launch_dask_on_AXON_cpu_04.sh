5f5de0161cd66ce0e22856b5f91a0ee4f10fd1dd
Arco Bast <arco.bast@caesar.de>
2019-08-23 13:57:50 +0200

    adapt to new cluster policy

--- SLURM_scripts/launch_dask_on_AXON_cpu_04.sh
+++ SLURM_scripts/launch_dask_on_AXON_cpu_04.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -p p.axon # partition (queue)
-#SBATCH -N 1 # number of nodes
-#SBATCH -n 1 # number of cores
+#SBATCH -N 4 # number of nodes
+#SBATCH -n 4 # number of cores
 #SBATCH --mem MaxMemPerNode # 100000 # memory pool for all cores
 #SBATCH -t 1-0:00 # time (D-HH:MM)
 #SBATCH -o out.slurm.%N.%j.slurm # STDOUT
@@ -10,5 +10,5 @@
 unset XDG_RUNTIME_DIR
 export SLURM_CPU_BIND=none
 ulimit -Sn "$(ulimit -Hn)"
-srun -n1 -N1 -c40 python $MYBASEDIR/project_src/in_silico_framework/SLURM_scripts/component_1.py $MYBASEDIR/management_dir_$1
+srun -n4 -N4 -c40 python $MYBASEDIR/project_src/in_silico_framework/SLURM_scripts/component_1.py $MYBASEDIR/management_dir_$1
 ## sleep 3000
