6658f9f9175e76f54086ded47a6c452cfcb392ea
Arco Bast <arco.bast@yahoo.de>
2019-09-14 14:36:11 +0200

    reintroduce caching but be aware of cell.evokedNW

--- simrun3/get_cell_with_network.py
+++ simrun3/get_cell_with_network.py
@@ -1,23 +1,5 @@
 import Interface as  I
-import warnings
 def get_cell_with_network(neuron_param, network_param, cache = True):
-    '''This returns a function, that returns a freshly initialized cell and network.
-    It takes advantage of the re_init methods, i.e. it is more efficient than 
-    recreating cell and network from scratch.'''
-    if not cache:
-        return get_cell_with_network_without_cache(neuron_param, network_param)
-    try:
-        neuron_param['neuron']['cell_modify_functions']['synaptic_input']
-        errstr = "Found cell_modify_function of type synaptic_input. "
-        errstr += "This is incompatible with cell caching. Caching "
-        errstr += "is therefore switched of. Please note: runtime will "
-        errstr += "increase."
-        warnings.warn(errstr)
-        return get_cell_with_network_without_cache(neuron_param, network_param)
-    except KeyError:
-        pass
-    
-    
     @I.cache
     def fun2():
         cell = I.scp.create_cell(neuron_param.neuron)  
@@ -30,17 +12,25 @@ def get_cell_with_network(neuron_param, network_param, cache = True):
         nwMap.re_init_network()
         for sec in cell.sections:
             sec._re_init_vm_recording()
-            sec._re_init_range_var_recording()        
-        return cell,nwMap
-    return fun
-
-def get_cell_with_network_without_cache(neuron_param, network_param):
-    '''This returns a function, that returns a freshly initialized cell and network.
-    It takes advantage of the re_init methods, i.e. it is more efficient than 
-    recreating cell and network from scratch.'''
-    def fun():
-        cell = I.scp.create_cell(neuron_param.neuron)  
-        nwMap = I.scp.NetworkMapper(cell, network_param.network)
+            sec._re_init_range_var_recording()      
+        # special case: a second evokedNW exists in cell.evokedNW
+        try:
+            cell.evokedNW.re_init_network()
+            errstr = "Found cell_modify_function of type synaptic_input. "
+            errstr += "Explicitly resetting cell.evokedNW."
+            errstr += "Please note: this is experimental. Please check the results."
+            print errstr
+            par = neuron_param['neuron']['cell_modify_functions']['synaptic_input']
+            # reactivate synapses
+            try:
+                # according to synapse activation file if it is specified in the neuron
+                synapse_activation_file = par['synapse_activation_file']
+                cell.evokedNW.reconnect_saved_synapses(synapse_activation_file)
+            except KeyError:
+                # according to network statistics if synapse acitvation file is not specified
+                cell.evokedNW.create_saved_network2()
+        except: # if cell.evokedNW does not exist:
+            pass # do nothing
         nwMap.create_saved_network2()
-        return cell, nwMap
+        return cell,nwMap
     return fun
