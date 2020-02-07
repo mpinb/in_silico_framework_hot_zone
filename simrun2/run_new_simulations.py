ac0591d8105689fb27a9add402693109eb87d77d
Rieke Fruengel <rieke.fruengel@gmail.com>
2020-02-07 17:33:55 +0100

    improved bugfix when using already built cell parameter file

--- simrun2/run_new_simulations.py
+++ simrun2/run_new_simulations.py
@@ -53,11 +53,9 @@ def _evoked_activity(cellParamName, evokedUpParamName, dirPrefix = '', \
     assert seed is not None
     np.random.seed(seed)
     print("seed: %i" % seed)
-    try:
-        neuronParameters = load_param_file_if_path_is_provided(resolve_mdb_path(cellParamName))
-    except AttributeError:
-        neuronParameters = load_param_file_if_path_is_provided(cellParamName)
-    evokedUpNWParameters = load_param_file_if_path_is_provided(resolve_mdb_path(evokedUpParamName)) ##sumatra function for reading in parameter file
+    if 
+    neuronParameters = load_param_file_if_path_is_provided(cellParamName)
+    evokedUpNWParameters = load_param_file_if_path_is_provided(evokedUpParamName) ##sumatra function for reading in parameter file
     scp.load_NMODL_parameters(neuronParameters)
     scp.load_NMODL_parameters(evokedUpNWParameters)
     cellParam = neuronParameters.neuron
