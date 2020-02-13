ab13e1087347e4a2295127fcf1494bb0f2e243ce
Rieke Fruengel <rieke.fruengel@gmail.com>
2020-02-13 12:54:30 +0100

    add ability to manually specify stim location in PW fitting

--- project_specific_ipynb_code/reproducing_L6paper.py
+++ project_specific_ipynb_code/reproducing_L6paper.py
@@ -547,17 +547,18 @@ def linear_interpolation_between_pairs(X,Y, x):
     return m*x+c    
 
 class PWfitting:
-    def __init__(self, l6_config, model_selection, min_time = 8, max_time = 25, mdb_path = '/nas1/Data_arco/results/20190114_spiketimes_database'):
+    def __init__(self, l6_config, model_selection, min_time = 8, max_time = 25, mdb_path = '/nas1/Data_arco/results/20190114_spiketimes_database', stim = 'D2'):
         self.l6_config = l6_config
         self.model_selection = model_selection
         self.min_time = min_time
         self.max_time = max_time
         self.mdb_path = mdb_path
+        self.stim = stim
         ### get target value
         #st_CDK = I.ModelDataBase('/nas1/Data_arco/results/20190114_spiketimes_database')['CDK_PassiveTouch'] # rieke
         st_CDK = I.ModelDataBase(self.mdb_path)['CDK_PassiveTouch']
         
-        st_CDK = I.select(st_CDK, stim = 'D2')
+        st_CDK = I.select(st_CDK, stim = self.stim) #changed by rieke
         self.CDK_target_value_avg_L5tt = I.temporal_binning(st_CDK, min_time = min_time, 
                                                        max_time = max_time, 
                                                        bin_size = max_time-min_time)[1][0]
