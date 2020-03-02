06014cf3485151800629e4321b19b4d104f519f5
Arco Bast <arco.bast@yahoo.de>
2020-03-02 17:45:39 +0100

    bugfix

--- single_cell_parser/cell_modify_functions/apical_epsp_injection.py
+++ single_cell_parser/cell_modify_functions/apical_epsp_injection.py
@@ -1,6 +1,6 @@
 from biophysics_fitting.setup_stim import setup_apical_epsp_injection as setup_apical_epsp_injection_
  
-def setup_apical_epsp_injection(cell, dist = None, amplitude = None, delay = None, rise = 1.0, decay = 5):
+def apical_epsp_injection(cell, dist = None, amplitude = None, delay = None, rise = 1.0, decay = 5):
     '''
     This injects epsp shaped current at a certain distance from the soma.
     '''
