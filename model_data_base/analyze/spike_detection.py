e6393d620e364b46e297573b09ddac07b996473b
Arco Bast <arco.bast@caesar.de>
2020-04-28 11:34:31 +0200

    allow specifying threashold for spike detection

--- model_data_base/analyze/spike_detection.py
+++ model_data_base/analyze/spike_detection.py
@@ -1,3 +1,4 @@
+from functools import partial
 import pandas as pd
 import dask
 import numpy as np
@@ -6,17 +7,18 @@ from single_cell_analyzer.membrane_potential_analysis import simple_spike_detect
 def spike_in_interval(st, tmin, tmax):
     return ((st>=tmin) & (st<tmax)).any(axis = 1)  
 
-def _helper(x):
+def _helper(x, threshold = 0):
     '''reads out a voltage trace, so it can be fed into simple_spike_detection()
     and converts the result back to pd.Series, so the result can be concatenated 
     to a dask dataframe'''
     t = x.index.values
     values = x.values
-    spikes = simple_spike_detection(t, values, mode = 'regular', threshold = 0)
+    spikes = simple_spike_detection(t, values, mode = 'regular', threshold = threshold)
     #print(len(spikes))
     return pd.Series({lv: x for lv, x in enumerate(spikes)})
 
-def spike_detection(ddf, get = None):
+def spike_detection(ddf, get = None, threshold = 0):
+    fun = partial(_helper, threshold = threshold)
     '''this method expects a dask dataframe and returns a pandas dataframe containing the spikes'''
     dummy = dask.compute(*map(dask.delayed(lambda x: x.apply(_helper, axis = 1)), ddf.to_delayed()), get = get)
     return pd.concat(dummy)
