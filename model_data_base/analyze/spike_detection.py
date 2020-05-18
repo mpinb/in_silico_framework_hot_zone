412b6ed82a9aeb99d88dff8520b29aa66db09827
Arco Bast <arco.bast@caesar.de>
2020-05-18 22:20:02 +0200

    bugfix: respect threshold

--- model_data_base/analyze/spike_detection.py
+++ model_data_base/analyze/spike_detection.py
@@ -20,5 +20,5 @@ def _helper(x, threshold = 0):
 def spike_detection(ddf, get = None, threshold = 0):
     fun = partial(_helper, threshold = threshold)
     '''this method expects a dask dataframe and returns a pandas dataframe containing the spikes'''
-    dummy = dask.compute(*map(dask.delayed(lambda x: x.apply(_helper, axis = 1)), ddf.to_delayed()), get = get)
+    dummy = dask.compute(*map(dask.delayed(lambda x: x.apply(fun, axis = 1)), ddf.to_delayed()), get = get)
     return pd.concat(dummy)
