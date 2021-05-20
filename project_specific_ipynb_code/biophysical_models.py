32a1ffe22e87b549b4817339b70f36440ad3c763
abast <arco.bast@caesar.de>
2021-05-20 13:22:53 +0200

    update plot_areas method

--- project_specific_ipynb_code/biophysical_models.py
+++ project_specific_ipynb_code/biophysical_models.py
@@ -480,34 +480,40 @@ class CurrentAnalysis:
         else:
             self.voltage_trace = None
         
-    def plot_areas(self, ax = None, normalized = False, plot_net = False, plot_voltage=False):
-        t = self.t
-        if ax is None:
-            fig = I.plt.figure(figsize = (10,4), dpi = 200)
-            ax = fig.add_subplot(111)
-        def __helper(currents, plot_label = True):
-            dummy = I.np.cumsum(currents, axis = 0)
-            dummy = I.np.vstack([I.np.zeros(dummy.shape[1]), dummy])
-            for lv, rv in enumerate(self.rangeVars):
-                ax.fill_between(t,dummy[lv,:], dummy[lv+1,:], 
-                                label = rv if plot_label else None,
-                                color = self.colormap[rv],
-                                linewidth = 0)
-        
-        if normalized:
-            __helper(self.depolarizing_currents_normalized)
-            __helper(self.hyperpolarizing_currents_normalized, False)
-            ax.plot(t, self.depolarizing_currents_sum+1, c = 'k')
-            ax.plot(t, self.hyperpolarizing_currents_sum-1, c = 'k')
+    def plot_areas(self, ax = None, normalized = False, plot_net = False, plot_voltage=False, t_stim = 295, select_window_relative_to_stim = (0,55)):
+            t = I.np.array(self.t) - t_stim
+            if ax is None:
+                fig = I.plt.figure(figsize = (10,4), dpi = 200)
+                ax = fig.add_subplot(111)
+            def __helper(currents, plot_label = True):
+                dummy = I.np.cumsum(currents, axis = 0)
+                dummy = I.np.vstack([I.np.zeros(dummy.shape[1]), dummy])
+                for lv, rv in enumerate(self.rangeVars):
+                    x,y1,y2 = t,dummy[lv,:],dummy[lv+1,:]
+                    select = (x>=select_window_relative_to_stim[0]) & (x<=select_window_relative_to_stim[1]) 
+                    x,y1,y2 = x[select],y1[select],y2[select]
+                    ax.fill_between(x,y1,y2, 
+                                    label = rv if plot_label else None,
+                                    color = self.colormap[rv],
+                                    linewidth = 0)
+            if normalized:
+                __helper(self.depolarizing_currents_normalized)
+                __helper(self.hyperpolarizing_currents_normalized, False)
+                ax.plot(t, self.depolarizing_currents_sum+1, c = 'k')
+                ax.plot(t, self.hyperpolarizing_currents_sum-1, c = 'k')
+            else:
+                __helper(self.depolarizing_currents)
+                __helper(self.hyperpolarizing_currents, False)
+            if plot_net:
+                ax.plot(t, self.net_current, 'k', label = 'net current')
+            if plot_voltage:
+                ax2 = ax.twinx()
+                select = (t>=select_window_relative_to_stim[0]) & (t<=select_window_relative_to_stim[1]) 
+                x,y = t[select], I.np.array(self.cell.soma.recVList[0])[select]
+                ax2.plot(x,y, 'k')
+                x,y = t[select], I.np.array(self.voltage_trace)[select]
+                ax2.plot(x,y, 'r')
 
-        else:
-            __helper(self.depolarizing_currents)
-            __helper(self.hyperpolarizing_currents, False)
-        if plot_net:
-            ax.plot(t, self.net_current, 'k', label = 'net current')
-        if plot_voltage:
-            ax2 = ax.twinx()
-            ax2.plot(t, self.voltage_trace, 'k')
             
     def plot_lines(self, ax = None, legend = True):
         if ax is None: 
