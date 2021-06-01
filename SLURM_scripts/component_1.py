49e75b03417c7e14263785adb5517ebbf15b50d7
abast <arco.bast@caesar.de>
2021-06-01 16:31:04 +0200

    works with Py2 startup script, 20210601

--- SLURM_scripts/component_1.py
+++ SLURM_scripts/component_1.py
@@ -13,7 +13,7 @@ import time
 # In[2]:
 
 management_dir = sys.argv[1]
-print('using management dir' , management_dir)
+print 'using management dir' , management_dir
 
 # In[3]:
 
@@ -56,7 +56,7 @@ def get_process_number():
                 x = int(x)
         with open(p, 'w') as f:
             f.write(str(x + 1))
-        print('I am process number {}'.format(x))
+        print 'I am process number {}'.format(x)
     return x
 
 def reset_process_number():
@@ -76,8 +76,8 @@ def get_locking_file_path():
     return os.path.join(management_dir, 'locking_server')
 
 def setup_locking_server():
-    print('-'*50)
-    print('setting up locking server')
+    print '-'*50
+    print 'setting up locking server'
     #command = 'redis-server --save "" --appendonly no --port 8885 --protected-mode no &'    
     #print command
     #os.system(command)
@@ -86,22 +86,22 @@ def setup_locking_server():
     with open(get_locking_file_path(), 'w') as f:
         f.write(yaml.dump(config))
     setup_locking_config()
-    print('-'*50)
+    print '-'*50
 
 def setup_locking_config():
-    print('-'*50)
-    print('updating locking configuration to use new server')
+    print '-'*50
+    print 'updating locking configuration to use new server'
     while not os.path.exists(get_locking_file_path()):
         time.sleep(1)
     os.environ['ISF_DISTRIBUTED_LOCK_CONFIG'] = get_locking_file_path() 
     check_locking_config()
-    print('-'*50)
+    print '-'*50
 
 def check_locking_config():
     import model_data_base.distributed_lock
-    print('locking configuration')
-    print(model_data_base.distributed_lock.server)
-    print(model_data_base.distributed_lock.client)
+    print 'locking configuration'
+    print model_data_base.distributed_lock.server
+    print model_data_base.distributed_lock.client
     #assert(model_data_base.distributed_lock.server['type'] == 'redis')
     #import socket
     #socket.gethostname()
@@ -132,62 +132,59 @@ def check_locking_config():
 # setting up dask-scheduler
 #################################################
 def _get_sfile(management_dir):
-    return os.path.join(management_dir, 'scheduler.json')
+    return os.path.join(management_dir, 'scheduler.json'), os.path.join(management_dir, 'scheduler3.json')
 
 def setup_dask_scheduler(management_dir):
-    print('-'*50)
-    print('setting up dask-scheduler')
-    sfile = _get_sfile(management_dir)
-    command = 'dask-scheduler --scheduler-file={} &'
+    print '-'*50
+    print 'setting up dask-scheduler'
+    sfile, sfile3 = _get_sfile(management_dir)
+    command = 'dask-scheduler --scheduler-file={} --port=28786 --bokeh-port=28787 &'
     command = command.format(sfile)
-    print(command)
+    print command
     os.system(command)
-    print('-'*50)
+    command = '''bash -ci "source ~/.bashrc; source_3; dask-scheduler --scheduler-file={} --port=38786 --dashboard-address=:38787" &'''
+    command = command.format(sfile3)
+    print command
+    os.system(command)
+    print '-'*50
 
-def setup_local_cluster():
-    print('-'*50)
-    print('setting up local dask-cluster')
-    import psutil
-    sfile = _get_sfile(management_dir) + '.' + str(process_number)
-    command = 'dask-scheduler --scheduler-file={} --port=4321 &'
-    command = command.format(sfile)
-    print(command)
-    os.system(command)
-    n_cpus = psutil.cpu_count(logical=False)
-    command = 'dask-worker --nthreads 1  --nprocs {nprocs} --scheduler-file={sfile} --memory-limit=100e9 &'.format(nprocs = n_cpus, sfile = sfile)
-    print('-'*50)
 # In[7]:
 
 #################################################
 # setting up dask-worker
 #################################################
 def setup_dask_workers(management_dir):
-    print('-'*50)
-    print('setting up dask-workers')
+    print '-'*50
+    print 'setting up dask-workers'
     import psutil
     n_cpus = psutil.cpu_count(logical=False)
-    sfile = _get_sfile(management_dir)
+    sfile, sfile3 = _get_sfile(management_dir)
     command = 'dask-worker --nthreads 1  --nprocs {nprocs} --scheduler-file={sfile} --memory-limit=100e9 &'.format(nprocs = n_cpus, sfile = sfile)
-
-    print(command)
+    print command
     os.system(command)
-    print('-'*50)
+    command = '''bash -ci "source ~/.bashrc; source_3; dask-worker --nthreads 1  --nprocs {nprocs} --scheduler-file={sfile} --memory-limit=100e9" &'''
+    command = command.format(nprocs = n_cpus, sfile = sfile3)
+    print command
+    os.system(command)
+    print '-'*50
 
 ##############################################
 # setting up jupyter-notebook
 #############################################
 def setup_jupyter_notebook():
-    print('-'*50)
-    print('setting up jupyter notebook')
+    print '-'*50
+    print 'setting up jupyter notebook'
     check_locking_config() 
     command = "cd notebooks; jupyter-notebook --ip='*' --no-browser --port=11112 &"
-    print(command)
+    print command
     os.system(command)    
-    print('-'*50)
-    command = "conda activate /axon/scratch/abast/anaconda3/; jupyter-lab --ip='*' --no-browser --port=11113 &"
-    command = 'screen -S jupyterlab -dm bash -c "source ~/.bashrc; source_3; ' +     '''jupyter-lab --ip='*' --no-browser --port=11113"'''
-    command = '''bash -c "source ~/.bashrc; source_3; jupyter-lab --ip='*' --no-browser --port=11113" &'''
-    command = "/axon/scratch/abast/anaconda3/bin/jupyter-lab --ip='*' --no-browser --port=11113"
+    print '-'*50
+    #command = "conda activate /axon/scratch/abast/anaconda3/; jupyter-lab --ip='*' --no-browser --port=11113 &"
+    #command = 'screen -S jupyterlab -dm bash -c "source ~/.bashrc; source_3; ' +     '''jupyter-lab --ip='*' --no-browser --port=11113"'''
+    #command = '''bash -c "source ~/.bashrc; source_3; jupyter-lab --ip='*' --no-browser --port=11113" &'''
+    #command = '''(source ~/.bashrc; source_3; jupyter-lab --ip='*' --no-browser --port=11113) &'''
+    command = '''bash -ci "source ~/.bashrc; source_3; jupyter-lab --ip='*' --no-browser --port=11113" &'''
+    #command = "/axon/scratch/abast/anaconda3/bin/jupyter-lab --ip='*' --no-browser --port=11113"
     os.system(command)
 # In[8]:
 
@@ -197,36 +194,6 @@ if process_number == 0:
     # setup_jupyter_notebook()
 setup_locking_config()
 setup_dask_workers(management_dir)
-#setup_local_cluster()
 if process_number == 0:
     setup_jupyter_notebook()
-#if not process_number == 0:
-#    # compute job will be started from first process
-#    time.sleep(60*60*24*365)
 time.sleep(60*60*24*365)
-
-#def iteratively_register_sub_mdbs(mdb):
-#    mdb._register_this_database()
-#    for k in mdb.keys():
-#        if mdb.metadata[k]['dumper'] == 'just_create_mdb':
-#            iteratively_register_sub_mdbs(mdb[k])
-#            
-#import Interface as I
-#import simrun3.robust_dask_delayed_execution
-#
-#mdb = I.ModelDataBase('/axon/scratch/abast/results/20190726_network_embedding_with_random_synapse_locations_for_L5tt_models')
-#iteratively_register_sub_mdbs(mdb)
-#
-#delayeds_db = mdb['delayeds_db']
-#
-#rde = simrun3.robust_dask_delayed_execution.RobustDaskDelayedExecution(delayeds_db)
-#
-#ds = rde.run_mdb()
-#
-#sfile =  _get_sfile(management_dir)
-#client = I.distributed.Client(scheduler_file=sfile)
-#
-#futures = client.compute(ds)
-#
-#time.sleep(60*60*24*7)
-### client.gather(futures)
