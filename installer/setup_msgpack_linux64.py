613e56402604baf44042035022446818c964293e
Omar Valerio <omar.valerio@gmail.com>
2021-12-19 20:27:59 +0100

    Custom ISF environment with pandas_msgpack support.

--- installer/setup_msgpack_linux64.py
+++ installer/setup_msgpack_linux64.py
@@ -1,6 +1,6 @@
 # IMPORTANT: Activate conda environment before running the script.
 import os
-basedir = os.path.dirname(__file__)
+basedir = os.getcwd()
 
 
 def system(x):
@@ -8,11 +8,10 @@ def system(x):
 
 
 # pandas_msgpack is currently the only package installed using setup.py
-names = sorted([f for f in os.listdir(basedir) if 'setup.py' in os.listdir(os.path.join(basedir, f))])
+names = [f for f in os.listdir(basedir) if os.path.isdir(f) and 'setup.py' in os.listdir(os.path.join(basedir, f))]
 commands = ['cd {}; python setup.py install'.format(os.path.join(basedir, n)) for n in names]
 
 for c in commands:
-    print('*'*20)
     print('[setup] {}'.format(c))
     system(c)
 
