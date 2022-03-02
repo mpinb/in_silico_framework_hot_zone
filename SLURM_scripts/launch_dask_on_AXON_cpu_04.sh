2da5270bfe33c3dd3b5b2d403c1a125f7e70c2b7
abast <arco.bast@caesar.de>
2022-03-02 14:20:53 +0100

    change memory to explicit values, required since SLURM update

--- SLURM_scripts/launch_dask_on_AXON_cpu_04.sh
+++ SLURM_scripts/launch_dask_on_AXON_cpu_04.sh
@@ -2,7 +2,7 @@
 #SBATCH -p p.axon # partition (queue)
 #SBATCH -N 4 # number of nodes
 #SBATCH -n 4 # number of cores
-#SBATCH --mem MaxMemPerNode # 100000 # memory pool for all cores
+#SBATCH --mem 384000 # 100000 # memory pool for all cores
 #SBATCH -t 1-0:00 # time (D-HH:MM)
 #SBATCH -o out.slurm.%N.%j.slurm # STDOUT
 #SBATCH -e err.slurm.%N.%j.slurm # STDERR
