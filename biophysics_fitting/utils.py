8102b48378fadc85e13af7fdc17c9d766f9b3bcb
Arco Bast <arco.bast@mpinb.mpg.de>
2022-03-22 16:47:21 +0100

    add code that allows to run functions in another python process without triggering an 'daemonic processes are not allowed to have children' error

--- biophysics_fitting/utils.py
+++ biophysics_fitting/utils.py
@@ -75,4 +75,96 @@ def vmApical_position(cell, dist = None):
     index = np.argmin(np.abs(np.array(sec.relPts) - target_x))
     return sec.pts[index]
 
+#########################################
+# multiprocessing stuff that allows to evaluate code in a separate python / NEURON environment
+#########################################
+
+import multiprocessing.process
+import cloudpickle
+
+class Undemonize(object):
+    '''A hack to resolve AssertionError: daemonic processes are not allowed to have children
+    
+    This might spawn child processes that do not terminate'''
+    def __init__(self):
+        self.p = multiprocessing.process.current_process()
+        if 'daemon' in self.p._config:
+            self.daemon_status_set = True
+        else:
+            self.daemon_status_set = False
+        self.daemon_status_value = self.p._config.get('daemon')
+    def __enter__(self):
+        if self.daemon_status_set:
+            del self.p._config['daemon']
+    def __exit__(self, type, value, traceback):
+        if self.daemon_status_set:
+            self.p._config['daemon'] = self.daemon_status_value
+import cloudpickle
+
+def run_cloudpickled_remotely(queue):
+    fun = cloudpickle.loads(queue.get())
+    args = cloudpickle.loads(queue.get())
+    queue.put(cloudpickle.dumps(fun(*args)))
+
+def execute_in_child_process(fun):
+    fun_cp = cloudpickle.dumps(fun)
+    def fun(*args):
+        queue = multiprocessing.Queue()
+        queue.put(fun_cp)
+        queue.put(cloudpickle.dumps(args))
+        p = multiprocessing.Process(target=run_cloudpickled_remotely, args=(queue,))
+        with Undemonize():    
+            p.start()
+        p.join()
+        out = cloudpickle.loads(queue.get())
+        return out
+    return fun
+
+# def run_cloudpickled_remotely3(queue_to_child, queue_from_child):
+#     fun = cloudpickle.loads(queue_to_child.get())
+#     while True:
+#         print('asd0')
+#         args = cloudpickle.loads(queue_to_child.get())
+#         print('asd1')
+#         queue_form_child.put(cloudpickle.dumps(fun(*args)))
+#         print('asd3')
+#         
+# def execute_in_child_process_kept_alive(fun):
+#     queue_to_child = multiprocessing.Queue()
+#     queue_from_child = multiprocessing.Queue()
+#     queue_to_child.put(cloudpickle.dumps(fun))
+#     p = multiprocessing.Process(target=test.run_cloudpickled_remotely3, 
+#                                 args=(queue_to_child,queue_from_child))
+#     with Undemonize():    
+#         p.start()    
+#     def fun(*args):
+#         queue_to_child.put(cloudpickle.dumps(args))
+#         out = cloudpickle.loads(queue_to_child.get())
+#         return out
+#     fun.process = p
+#     return fun
+
+from functools import partial
+
+def pool_helper(callable_partial):
+    callable_partial = cloudpickle.loads(callable_partial)
+    return cloudpickle.dumps(callable_partial())
+
+class VariableThatDoesNotGetPickled:
+    def __init__(self, stored_item):
+        self.stored_item = stored_item
+    def __getstate__(self):
+        return {'stored_item': None}
+    
+def execute_in_child_process_kept_alive(fun):
+    pool_storage = VariableThatDoesNotGetPickled(None) # None # multiprocessing.Pool(1) 
+    def _helper(*args):
+        if pool_storage.stored_item is None:
+            with Undemonize():
+                pool_storage.stored_item = multiprocessing.Pool(1)
+        p = cloudpickle.dumps(partial(fun, *args))
+        out = pool_storage.stored_item.map(pool_helper, [p])[0]
+        return cloudpickle.loads(out)
+    #fun.pool = pool
+    return _helper
 
