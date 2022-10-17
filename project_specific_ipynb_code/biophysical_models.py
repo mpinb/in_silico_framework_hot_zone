0f508d7dd7ec12f98abe83efcecd83bdc55aa5df
Arco Bast <arco.bast@mpinb.mpg.de>
2022-10-17 15:42:36 +0200

    add visualize_vt function

--- project_specific_ipynb_code/biophysical_models.py
+++ project_specific_ipynb_code/biophysical_models.py
@@ -635,3 +635,56 @@ def interpolate_voltage_trace(voltage_trace_):
 import six 
 def interpolate_voltage_traces(voltage_traces_):
     return {k: interpolate_voltage_trace(v) for k, v in six.iteritems(voltage_traces_)}
+
+
+###################################
+
+def visualize_vt(vt):
+    plt = I.plt
+    fig = plt.figure(dpi = 200, figsize = (8,6))
+    ax = fig.add_subplot(2,2,1)
+    t = vt['BAC.hay_measure']['tVec']
+    vs = vt['BAC.hay_measure']['vList']
+    select = (t >= 295-10) & (t < 295+480)
+    ax.plot(t[select]-295,vs[0][select], 'k')
+    # ax.plot(t[select]-295,vs[2][select], '#f7941d')
+    ax.plot(t[select]-295,vs[1][select], '#f7941d')
+    ax.plot([20,40],[30,30])
+    ax.plot([50,50],[30,10])
+
+    ax = fig.add_subplot(2,2,2)
+    t = vt['bAP.hay_measure']['tVec']
+    vs = vt['bAP.hay_measure']['vList']
+    select = (t >= 295-10) & (t < 295+80)
+    ax.plot(t[select]-295,vs[0][select], 'k')
+    ax.plot(t[select]-295,vs[2][select], '#f7941d')
+    ax.plot([20,40],[30,30])
+    ax.plot([50,50],[30,10])
+
+    ax = fig.add_subplot(2,3,4)
+    t = vt['StepOne.hay_measure']['tVec']
+    vs = vt['StepOne.hay_measure']['vList']
+    select = (t >= 600) & (t < 2800)
+    ax.plot(t[select]-700,vs[0][select], 'k')
+    ax.plot([500,1500],[30,30])
+    ax.plot([2050,2050],[30,10])
+
+    ax = fig.add_subplot(2,3,5)
+    t = vt['StepTwo.hay_measure']['tVec']
+    vs = vt['StepTwo.hay_measure']['vList']
+    select = (t >= 600) & (t < 2800)
+    ax.plot(t[select]-700,vs[0][select], 'k')
+    ax.plot([500,1500],[30,30])
+    ax.plot([2050,2050],[30,10])
+
+    ax = fig.add_subplot(2,3,6)
+    t = vt['StepThree.hay_measure']['tVec']
+    vs = vt['StepThree.hay_measure']['vList']
+    select = (t >= 600) & (t < 2800)
+    ax.plot(t[select]-700,vs[0][select], 'k')
+    ax.plot([500,1500],[30,30])
+    ax.plot([2050,2050],[30,10])
+
+    for ax in fig.axes:
+        ax.set_ylim(-90,40)
+        ax.axis('off')
