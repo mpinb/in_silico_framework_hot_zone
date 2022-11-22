b35fbecaf6a59436061ac20eda1fc72ebec19684
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2022-11-22 14:54:32 +0100

    General bug fixes to general submit script and jupyter link script. Jupyter link now checks if node is running properly.

--- SLURM_scripts/submit.sh
+++ SLURM_scripts/submit.sh
@@ -1,94 +1,6 @@
 #!/bin/bash -l
-help() {
-    cat <<EOF
-  Usage: $0 {job name} [options]
-
-  Arguments:
-
-  name
-    Name of the job
-    Required
-
-  -h, --help
-    Display this usage message and exit.
-
-  -N <val>
-    Amount of nodes
-    Default: 1
-
-  -n <val>
-    Amount of cores
-    Default: 48
-
-  -m <val>
-    Amount of memory (in MB)
-    default: 300,000 MB
-
-  -t <val>
-    Amount of time
-    format: "d-h:m"
-
-  -g
-    Request a GPU partition.
-
-  -i
-    Launch an interactive job.
-
-  -p <val>
-    Request a specific partition. Overwrites the -i and -g flags.
-    Default: "CPU"
-    Options: [CPU, CPU-interactive, GPU, GPU-interactive, GPU-a100]
-
-  -A
-    Request resources on the A100 nodes.
-    Overwrites the -g, -i and -p flags.
-
-  -T <val>
-    Amount of tasks per node
-    Default: 20
-
-  -r <val>
-    Set value for gres.
-    Default:
-      1 for CPU jobs and non-interactive GPU jobs
-      4 for interactive GPU jobs
-EOF
-}
-
-usage() {
-cat << EOF
-  Usage: $0 {job name} [options]
-EOF
-}
-
-qos_setting() {
-  if [ $1 = "GPU-a100" ]
-  then
-    echo "\n#SBATCH\n--qos=GPU-a100"
-  else
-    echo 
-  fi
-}
-
-function args_precheck {
-  if [ $1 -eq "0" ] ; then
-    echo "Warning: no arguments passed. Will launch a job with default parameters and no name."
-  elif [ $2 = "--help" ] || [ $2 = "-h" ] ; then
-    help
-    exit 0
-  elif [[ $2 == -* ]] ; then
-    echo "Warning: provided parameters before a job name. You must provide a job name first."
-    usage
-    exit 1
-  fi
-}
-
-
-args_precheck $# $1;
 
 # Default options
-name=$1
-shift;
 nodes="1"
 partition="CPU"
 cores="48"
@@ -99,34 +11,117 @@ gres="0"  # unset
 a100=""  # unset
 qos=""  # unset
 
+help() {
+  cat <<EOF
+    Usage: $0 {job name} [options]
+
+    Arguments:
+
+    name
+      Name of the job
+      Required
+
+    -h, --help
+      Display this usage message and exit.
+
+    -N <val>
+      Amount of nodes
+      Default: $nodes
+
+    -n <val>
+      Amount of cores
+      Default: $cores
+
+    -m <val>
+      Amount of memory (in MB)
+      default: $mem MB
+
+    -t <val>
+      Amount of time
+      Format: "d-h:m"
+      default: $time
+
+    -g
+      Request a GPU partition.
+
+    -i
+      Launch an interactive job.
+
+    -p <val>
+      Request a specific partition. Overwrites the -i and -g flags.
+      Default: $partition
+      Options: [CPU, CPU-interactive, GPU, GPU-interactive, GPU-a100]
+
+    -A
+      Request resources on the A100 nodes.
+      Overwrites the -g, -i and -p flags.
+
+    -T <val>
+      Amount of tasks per node
+      Default: $tpn
+
+    -r <val>
+      Set value for gres.
+      Default:
+        $gres for CPU jobs and non-interactive GPU jobs
+        4 for interactive GPU jobs
+EOF
+}
+
+usage() {
+  cat << EOF
+  Usage: $0 [options] {job name}
+EOF
+}
+
+qos_setting() {
+  if [ "$1" == "GPU-a100" ]
+  then
+    echo "\n#SBATCH\n--qos=GPU-a100"
+  else
+    echo ""
+  fi
+}
+
+function args_precheck {
+  if [ $1 -eq "0" ] ; then
+    echo "Warning: no arguments passed. Will launch a job with default parameters and no name."
+  elif [ $2 = "--help" ] || [ $2 = "-h" ] ; then
+    help
+    exit 0
+  fi
+}
+
+args_precheck $# $1;
+
 # Parse options
-while getopts hN:p:n:m:t:giA:T:r: OPT
+while getopts hN:p:n:m:t:A:T:r:gi OPT;
 do
-    case "$OPT" in
-        h) usage
-           exit;;
-        N) nodes=${OPTARG};;
-        n) cores=${OPTARG};;
-        m) mem=${OPTARG};;
-	      t) time=${OPTARG};;
-	      g) partition="GPU";;
-	      i) partition=$partition"-interactive";;  # append "-interactive"
-        p) partition=${OPTARG};;  # overwrites i or g flag
-        T) tpn=${OPTARG};;
-        r) gres=${OPTARG};;
-        A) a100="_"${OPTARG}; qos="GPU-a100";;  # appendix to start the correct python file
-        \?) # incorrect option
-         echo "Error: Invalid option"
-         exit 1;;
-    esac
+  case "$OPT" in
+    h) usage
+        exit;;
+    N) nodes=${OPTARG};;
+    n) cores=${OPTARG};;
+    m) mem=${OPTARG};;
+    t) time=${OPTARG};;
+    g) partition="GPU";;
+    i) partition=$partition"-interactive";;  # append "-interactive"
+    p) partition=${OPTARG};;  # overwrites i or g flag
+    T) tpn=${OPTARG};;
+    r) gres=${OPTARG};;
+    A) a100="_"${OPTARG}; qos="GPU-a100";;  # appendix to start the correct python file
+    \?) # incorrect option
+      echo "Error: Invalid option"
+      exit 1;;
+  esac
 done
-
-qosline = qos_setting $a100
-
-shift $((OPTIND-1))  # just good practice
+shift $(( OPTIND - 1 ))  # shift the option index to point to the first non-option argument (should be name of the job)
+name=$1
+shift;
+qosline="$(qos_setting $a100)"
 
 if [ $partition = "GPU-interactive" ] && [ $gres -eq "0" ]; then  # set gres to 4 for GPU-interactive jobs if it wasn't passed in command line
-  gres = "4"
+  gres="4"
 fi
 
 # Print out job information
@@ -135,11 +130,13 @@ echo "
 Launching job named \""$name"\" on $partition with
 - $nodes nodes
 - $cores cores 
-- $mem memory
+- $mem MB memory
+- gres: $gres
 - for $time
 "
 
 # create wrapper script to start batch job with given parameters
+# using EoF or EoT makes it easier to pass multi-line text with argument values
 sbatch << EoT
 #!/bin/sh
 #SBATCH --job-name=$name
