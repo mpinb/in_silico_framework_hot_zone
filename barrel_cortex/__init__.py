2fe3805425d346670a729b5a10cdf66d0cf5b19b
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-01-11 15:28:02 +0100

    Moved various files that are not modules to etc/ Created barrel_cortex module with barrel cortex related files Created script to archive branches in etc/

---
+++ barrel_cortex/__init__.py
@@ -0,0 +1,10 @@
+"""Initialisation file for the barrel_cortex module
+
+This module should contain everything that's related to the entire barrel cortex.
+It includes:
+    - average_barrel_field_L45_border.am: barrel cortex geometry, to be rendered in AMIRA
+    - barrel_cortex.py: contains functions specific to barrel cortex simulations, such as synaptic strength readout, methods to move morphologies in barrel cortex...
+
+Author: Bjorge Meulemeester
+Date: 10/01/2023
+"""
