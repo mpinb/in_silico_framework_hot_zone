35df18bd36d06c4c1a9c92fec77de2643ee307ac
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-01-17 15:29:30 +0100

    Bug fix

--- etc/SLURM_scripts/component_1_SOMA.py
+++ etc/SLURM_scripts/component_1_SOMA.py
@@ -64,7 +64,7 @@ def get_process_number():
                 x = int(x)
         with open(p, 'w') as f:
             f.write(str(x + 1))
-        print('I am process number {}').format(x)
+        print('I am process number {}'.format(x))
     return x
 
 def reset_process_number():
