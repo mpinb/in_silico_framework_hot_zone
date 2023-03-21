e02dff9c43296c818fb3c9cad3eb55dd5c9d01b5
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-03-21 17:32:32 +0100

    Removed authorship as I don't know the other authors

--- barrel_cortex/__init__.py
+++ barrel_cortex/__init__.py
@@ -5,7 +5,6 @@ It includes:
     - average_barrel_field_L45_border.am: barrel cortex geometry, to be rendered in AMIRA
     - barrel_cortex.py: contains functions specific to barrel cortex simulations, such as synaptic strength readout, methods to move morphologies in barrel cortex...
 
-Author: Bjorge Meulemeester
 Date: 10/01/2023
 """
 import pandas as pd
