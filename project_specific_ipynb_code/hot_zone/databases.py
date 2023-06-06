ceb819dde1d8e198f8a5c034ca136903fe4f0f8f
Arco Bast <arco.bast@mpinb.mpg.de>
2023-06-06 17:38:38 +0200

    keep track of all data bases used in the hot zone project

---
+++ project_specific_ipynb_code/hot_zone/databases.py
@@ -0,0 +1,11 @@
+# Example trials with fitted sustained
+
+# spike times
+mdb_VPM = I.ModelDataBase('/gpfs/soma_fs/ibs/current_data/Data_arco/results/20191202_rajeev_ongoing_VPM')
+mdb_RN_BC = I.ModelDataBase('/gpfs/soma_fs/ibs/current_data/Data_arco/results/20210520_analyze_rajeevs_barrel_cortex_air_puff_responses_for_hot_zone_paper/')
+mdb_MG = I.ModelDataBase('/gpfs/soma_fs/ibs/current_data/Data_arco/results/20200117_analysis_of_led_and_airpuff_recordings_paper_review')
+
+
+# example simulation trials for figure 3 with fitted sustained activity
+mdb_fig3_with_sustained = I.ModelDataBase('/gpfs/soma_fs/scratch/abast/results/20230312_hot_zone_figure_3_v2')
+
