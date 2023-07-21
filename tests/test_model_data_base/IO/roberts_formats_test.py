e51ad2d323fe538066c87456d6cbd3646873c351
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-07-21 14:41:30 +0200

    use pytests tmpdir for making tempdirs instead of managing it yourself

--- tests/test_model_data_base/IO/roberts_formats_test.py
+++ tests/test_model_data_base/IO/roberts_formats_test.py
@@ -12,7 +12,7 @@ import os
 
 
 
-class TestRobertsFormats(unittest.TestCase):          
+class TestRobertsFormats:          
     def setUp(self):
         self.synapse_file_path = os.path.join(test_data_folder, \
                                               '20150815-1530_20240', \
@@ -20,18 +20,15 @@ class TestRobertsFormats(unittest.TestCase):
         
         self.assertTrue(os.path.exists(self.synapse_file_path)) 
         
-    def test_saved_and_reloded_synapse_file_is_identical(self):
+    def test_saved_and_reloded_synapse_file_is_identical(self, tmpdir):
         synapse_pdf = read_pandas_synapse_activation_from_roberts_format(\
                              self.synapse_file_path, sim_trail_index = 'asdasd')        
         
         try:
-            path = tempfile.mkdtemp()
-            path_file = os.path.join(path, 'test.csv')
+            path_file = os.path.join(tmpdir.dirname, 'test.csv')
             write_pandas_synapse_activation_to_roberts_format(path_file, synapse_pdf)
             synapse_pdf_reloaded = read_pandas_synapse_activation_from_roberts_format(path_file, sim_trail_index = 'asdasd') 
         except:
             raise
-        finally:
-            shutil.rmtree(path)
             
         assert_frame_equal(synapse_pdf.dropna(axis=1, how = 'all'), synapse_pdf_reloaded.dropna(axis=1, how = 'all'))
