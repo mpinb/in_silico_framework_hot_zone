1a79a634e653a89e1eb442c5005cdf6a457216ff
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-08-03 15:03:03 +0200

    Don't align trunk in testing for py2

--- tests/test_visualisation/cell_morphology_visualizer_test.py
+++ tests/test_visualisation/cell_morphology_visualizer_test.py
@@ -1,21 +1,16 @@
 from visualize.cell_morphology_visualizer import CellMorphologyVisualizer
-import single_cell_parser as scp
-import neuron
-h = neuron.h
-import os, unittest
 from .context import *
-from getting_started import getting_started_dir # path to getting started folder
 from single_cell_parser.serialize_cell import *
-from model_data_base.utils import silence_stdout
 from tests import setup_current_injection_experiment
+import six
 
-class TestCellMorphologyVisualizer(unittest.TestCase):
-    def setUp(self):
+class TestCellMorphologyVisualizer:
+    def __init__(self):
         self.cell = setup_current_injection_experiment()
         self.cv = None
 
     def test_init(self):
-        self.cv = CellMorphologyVisualizer(self.cell)
+        self.cv = CellMorphologyVisualizer(self.cell, align_trunk=six.PY3)  # don't align trunk in py2, ithas no scipy Rotation object
         self.assertIsInstance(self.cv, CellMorphologyVisualizer)
 
     # TODO: add more tests
