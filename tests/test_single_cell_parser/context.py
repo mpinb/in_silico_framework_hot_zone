62e3d9b5cf2609d79e24f2966d920b5c156b6c39
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-08-21 16:40:20 +0200

    Moved network_param_modify_functions tests to tests module

--- tests/test_single_cell_parser/context.py
+++ tests/test_single_cell_parser/context.py
@@ -3,8 +3,10 @@ import os
 import sys
 import tempfile
 import getting_started
+import neuron
+h = neuron.h
 
 parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 this_folder = os.path.abspath(os.path.dirname(__file__))
-
+fname = 'data/85.hoc'
 sys.path.insert(0, parent)
