230ee33d18218cdad0a90a16536b0957f1c9124c
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-08-21 17:57:45 +0200

    fix parameters error Pass None, as they are only used in rieke_spines

--- tests/test_single_cell_parser/cell_parser_test.py
+++ tests/test_single_cell_parser/cell_parser_test.py
@@ -3,7 +3,7 @@ from .context import h, fname
 
 def test_cell_parser():
     testParser = CellParser(fname)
-    testParser.spatialgraph_to_cell()
+    testParser.spatialgraph_to_cell(parameters=None)
     testParser.insert_passive_membrane('Soma')
     testParser.insert_passive_membrane('Dendrite')
     testParser.insert_passive_membrane('ApicalDendrite')
