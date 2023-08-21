4eeca0748315a0e8eae7c5d06b54e8bbb488d3a8
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-08-21 16:43:10 +0200

    moved cell_parser tests to tests module

---
+++ tests/test_single_cell_parser/cell_parser_test.py
@@ -0,0 +1,16 @@
+from single_cell_parser.cell_parser import CellParser
+from context import h, fname
+
+def test_cell_parser():
+    testParser = CellParser(fname)
+    testParser.spatialgraph_to_cell()
+    testParser.insert_passive_membrane('Soma')
+    testParser.insert_passive_membrane('Dendrite')
+    testParser.insert_passive_membrane('ApicalDendrite')
+    testParser.insert_hh_membrane('Soma')
+    testParser.insert_hh_membrane('Dendrite')
+    testParser.insert_hh_membrane('ApicalDendrite')
+    for label in list(testParser.cell.branches.keys()):
+        for branch in testParser.cell.branches[label]:
+            for sec in branch:
+                h.psection(sec=sec)
