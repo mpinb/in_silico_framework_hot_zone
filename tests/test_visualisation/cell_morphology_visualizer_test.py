ffa555c6a2e17df86b40148abc8d7a18e692c513
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-08-29 19:11:36 +0200

    skipif reason string

--- tests/test_visualisation/cell_morphology_visualizer_test.py
+++ tests/test_visualisation/cell_morphology_visualizer_test.py
@@ -6,34 +6,32 @@ import six
 import pytest
 import distributed
 
-@pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
 class TestCellMorphologyVisualizer:
-    @pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
     def setup_class(self):
         self.cell = setup_current_injection_experiment()
         self.cmv = CellMorphologyVisualizer(self.cell, align_trunk=six.PY3)  # don't align trunk in py2, ithas no scipy Rotation object
         self.t_start, self.t_end, self.t_step = self.cmv.t_start, self.cmv.t_end, (self.cmv.t_end-self.cmv.t_start)//2  # a two-frame gif
     
-    @pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="The cell_morphology_visualizer methods are not available on Py2")
     def test_init(self):
         assert isinstance(self.cmv, CellMorphologyVisualizer)
 
-    @pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="The cell_morphology_visualizer methods are not available on Py2")
     def test_show_morphology_3d(self):
         self.cmv.show_morphology_3d(highlight_section=1)
 
-    @pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="The cell_morphology_visualizer methods are not available on Py2")
     def test_show_voltage_in_morphology_3d(self):
         self.cmv.show_voltage_in_morphology_3d(time_point=0, highlight_section=1)
 
-    @pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="The cell_morphology_visualizer methods are not available on Py2")
     def test_show_voltage_synapses_in_morphology_3d(self):
         """
         TODO: test highlight arrow?
         """
         self.cmv.show_voltage_synapses_in_morphology_3d(time_point=0)
 
-    @pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="The cell_morphology_visualizer methods are not available on Py2")
     def test_write_gif_voltage_synapses_in_morphology_3d(self, tmpdir):
         outdir = str(tmpdir.dirname)
         self.cmv.write_gif_voltage_synapses_in_morphology_3d(
@@ -42,7 +40,7 @@ class TestCellMorphologyVisualizer:
             client=distributed.client_object_duck_typed
             )
 
-    @pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="The cell_morphology_visualizer methods are not available on Py2")
     def test_write_video_voltage_synapses_in_morphology_3d(self, tmpdir):
         outdir = str(tmpdir.dirname)
         self.cmv.write_video_voltage_synapses_in_morphology_3d(
@@ -51,7 +49,7 @@ class TestCellMorphologyVisualizer:
             client=distributed.client_object_duck_typed
         )
 
-    @pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="The cell_morphology_visualizer methods are not available on Py2")
     def test_display_animation_voltage_synapses_in_morphology_3d(self, tmpdir):
         outdir = tmpdir.dirname
         self.cmv.display_animation_voltage_synapses_in_morphology_3d(
@@ -59,21 +57,19 @@ class TestCellMorphologyVisualizer:
             client=distributed.client_object_duck_typed
         )
 
-@pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
 class TestCellMorphologyInteractiveVisualizer:
-    @pytest.mark.skipif(six.PY2, "The cell_morphology_visualizer methods are not available on Py2")
     def setup_class(self):
         self.cell = setup_current_injection_experiment(rangevars=['NaTa_t.ina'])
         self.cmiv = CellMorphologyInteractiveVisualizer(cell=self.cell, align_trunk=six.PY3)
     
-    @pytest.mark.skipif(six.PY2, "Interactive visualizations are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="Interactive visualizations are not available on Py2")
     def test_display_interactive_morphology_3d(self):
         self.cmiv.display_interactive_morphology_3d()
 
-    @pytest.mark.skipif(six.PY2, "Interactive visualizations are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="Interactive visualizations are not available on Py2")
     def test_display_interactive_voltage_in_morphology_3d(self):
         self.cmiv.display_interactive_voltage_in_morphology_3d()
 
-    @pytest.mark.skipif(six.PY2, "Interactive visualizations are not available on Py2")
+    @pytest.mark.skipif(six.PY2, reason="Interactive visualizations are not available on Py2")
     def test_display_interactive_ion_dynamics_in_morphology_3d(self):
         self.cmiv.display_interactive_ion_dynamics_in_morphology_3d(ion_keyword='NaTa_t.ina')
