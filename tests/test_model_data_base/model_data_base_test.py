bfd4b5be25013e229c3a5b860db428bd27fea3ae
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-09-04 13:23:03 +0200

    Fix gitignore

--- tests/test_model_data_base/model_data_base_test.py
+++ tests/test_model_data_base/model_data_base_test.py
@@ -85,12 +85,16 @@ class TestModelDataBase(unittest.TestCase):
         self.fresh_mdb.setitem('test', 1, dumper = 'self')
         self.fresh_mdb.setitem('test2', 1, dumper = to_pickle)
         git_status = subprocess.check_output(['git status'], shell=True).decode('utf-8')
+        msg="{} =/= {}\n"+
+        "Did the commit turn dirty during testing?\n{}".format(
+            self.fresh_mdb.metadata['test']['version'], 
+            model_data_base.get_version()['version'], git_status)
         self.assertEqual(self.fresh_mdb.metadata['test']['version'], \
                          model_data_base.get_versions()['version'],
-                         msg=git_status)
+                         msg=msg)
         self.assertEqual(self.fresh_mdb.metadata['test2']['version'], \
                          model_data_base.get_versions()['version'],
-                         msg=git_status)
+                         msg=msg)
         self.assertEqual(self.fresh_mdb.metadata['test']['dumper'], 'self')
         self.assertEqual(self.fresh_mdb.metadata['test2']['dumper'], 'to_pickle')
         self.assertEqual(self.fresh_mdb.metadata['test']['metadata_creation_time'], \
