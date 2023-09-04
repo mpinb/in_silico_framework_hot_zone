7f1a12c09d24b45e56ce5e8c67b4cae3a54c4d66
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-09-04 12:51:01 +0200

    Dont set log level in Interface Otherwise it will reset every time Interface gets imported

--- Interface.py
+++ Interface.py
@@ -42,12 +42,11 @@ import six
 
 ### logging setup
 import logging
-# All loggers will inherit the root logger's level and handlers
-root_logger = logging.getLogger()
-root_logger.setLevel(logging.INFO)  # set logging level of root logger to INFO
+# All loggers will inherit this root logger's level and handlers
+logger = logging.getLogger()
 log_stream_handler = logging.StreamHandler(stream=sys.stdout)  # a singular stream handler, so that all logs can redirect to this one
 log_stream_handler.setFormatter(logging.Formatter("[%(levelname)s] %(name)s: %(message)s"))
-root_logger.handlers = [log_stream_handler]
+logger.handlers = [log_stream_handler]
 
 try:
     from IPython import display
