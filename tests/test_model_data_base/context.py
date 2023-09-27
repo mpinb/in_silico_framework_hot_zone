504646e85abc91fa5aba8df09f7f997dd7ac57cd
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-09-27 17:23:02 +0200

    make pytest fixture for fresh mdb

--- tests/test_model_data_base/context.py
+++ tests/test_model_data_base/context.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import os, sys, shutil, tempfile
 import distributed
+import pytest
 parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.insert(0, parent)
 
