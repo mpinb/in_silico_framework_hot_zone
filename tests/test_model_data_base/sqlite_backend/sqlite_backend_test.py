9027cf2b8036ec8c2e00d8c622d160a8f783011d
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-09-28 18:49:02 +0200

    sqlite db as fixture

--- tests/test_model_data_base/sqlite_backend/sqlite_backend_test.py
+++ tests/test_model_data_base/sqlite_backend/sqlite_backend_test.py
@@ -14,55 +14,41 @@ from model_data_base.sqlite_backend.sqlite_backend import SQLiteBackend as Sqlit
 def write_data_to_dict(path, key):
     dict_ = SqliteDict(path)
     data = np.ones(shape = (1000,1000))*int(key)
-    dict_[key] = data
-            
-class TestSQLiteBackend:
-    def setup_class(self):
-        self.tempdir = tempfile.mkdtemp()
-        self.path = os.path.join(self.tempdir, 'tuplecloudsql_test.db')
-        self.db = SqliteDict(self.path)
+    dict_[key] = data    
+
+def test_str_values_can_be_assigned(sqlite_db):
+    db = sqlite_db
+    db['test'] = 'test'
+    assert db['test'] == 'test'
+
+def test_tuple_values_can_be_assigned(sqlite_db):
+    db = sqlite_db
+    db[('test',)] = 'test'
+    assert db[('test',)] == 'test'  
+    db[('test','abc')] = 'test2'
+    assert db[('test','abc')] == 'test2'  
+
+def test_pixelObject_can_be_assigned(sqlite_db):
+    db = sqlite_db
+    #plot figure and convert it to PixelObject
+    import matplotlib.pyplot as plt
+    from visualize._figure_array_converter import PixelObject
+    fig = plt.figure()
+    fig.add_subplot(111).plot([1,5,3,4])
+    po = PixelObject([0, 10, 0, 10], fig = fig)
         
-    def teardown_class(self):
-        if os.path.exists(self.tempdir):
-            shutil.rmtree(self.tempdir)        
-    
-    def test_str_values_can_be_assigned(self):
-        db = self.db
-        db['test'] = 'test'
-        assert db['test'] == 'test'
-    
-    def test_tuple_values_can_be_assigned(self):
-        db = self.db
-        db[('test',)] = 'test'
-        assert db[('test',)] == 'test'  
-        db[('test','abc')] = 'test2'
-        assert db[('test','abc')] == 'test2'  
-    
-    def test_pixelObject_can_be_assigned(self):
-        db = self.db
-        #plot figure and convert it to PixelObject
-        import matplotlib.pyplot as plt
-        from visualize._figure_array_converter import PixelObject
-        fig = plt.figure()
-        fig.add_subplot(111).plot([1,5,3,4])
-        po = PixelObject([0, 10, 0, 10], fig = fig)
-         
-        #save and reload PixelObject
-        db[('test', 'myPixelObject')] = po
-        po_reconstructed = db[('test', 'myPixelObject')]
-    
-    #@decorators.testlevel(1)  
-    def test_concurrent_writes(self, client):
-        keys = [str(lv) for lv in range(100)]
-        job = {key: write_data_to_dict(self.path, key) for key in keys}
-        job = dask.delayed(job)
-         
-        client.compute(job).result()
-         
-        assert(set(self.db.keys()) == set(keys))
-        for k in keys:
-            np.testing.assert_equal(self.db[k][0,0], int(k))
-            
+    #save and reload PixelObject
+    db[('test', 'myPixelObject')] = po
+    po_reconstructed = db[('test', 'myPixelObject')]
+
+#@decorators.testlevel(1)  
+def test_concurrent_writes(sqlite_db, client):
+    keys = [str(lv) for lv in range(100)]
+    job = {key: write_data_to_dict(sqlite_db.basedir, key) for key in keys}
+    job = dask.delayed(job)
         
+    client.compute(job).result()
         
-        
+    assert(set(sqlite_db.keys()) == set(keys))
+    for k in keys:
+        np.testing.assert_equal(sqlite_db[k][0,0], int(k))
