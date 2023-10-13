0de8b1272dc218b89546b3bfd3a27852896e0dce
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-10-13 13:05:20 +0200

    skip dask test to see why py2 errors

--- tests/test_model_data_base/dask_test.py
+++ tests/test_model_data_base/dask_test.py
@@ -12,7 +12,8 @@ def get_ddf():
     '''constructs dask dataframe out of pandas dataframes via the .from-delayed method with indexes A1, A2, A3, ... F3, F3, F4'''
     delayed_list = [dask.delayed(get_pdf)(x) for x in 'ABCDEF']  
     return dask.dataframe.from_delayed(delayed_list)
-  
+
+@pytest.mark.skipif(six.PY2, reason="Skip to see why Py2 errors - TEMPORARY, pls fix")
 def test_join_operation_of_dask(client):
     '''Tests the join operation of dask. Should be ok if dask >= 0.10.2
     Compare https://stackoverflow.com/questions/38416836/result-of-join-in-dask-dataframes-seems-to-depend-on-the-way-the-dask-datafram'''
