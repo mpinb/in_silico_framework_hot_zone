49f7b67ee5a93c22b25e4b5e110cbb93577d4f09
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-11-07 16:27:33 +0100

    update jupylink

--- SLURM_scripts/jupyter_link.sh
+++ SLURM_scripts/jupyter_link.sh
@@ -10,7 +10,9 @@ QOS=""
 
 # The user-defined port numbers of jupyter notebook/lab
 __LOCATION__="$(dirname "$(realpath "$0")")"
-DASK_PORT=$(awk -F "=" '/dask_client_2/ {print $2}' $__LOCATION__/user_settings.ini)
+DASK_PORT=$(awk -F "=" '/dask_client_3/ {print $2}' $__LOCATION__/user_settings.ini)
+NOTEBOOK_PORT=$(awk -F "=" '/jupyter_notebook/ {print $2}' $__LOCATION__/user_settings.ini)
+LAB_PORT=$(awk -F "=" '/jupyter_lab/ {print $2}' $__LOCATION__/user_settings.ini)
 
 NC='\033[0m' # No Color
 ORANGE='\033[0;33m' # orange color
@@ -41,35 +43,6 @@ function printf_with_dots {
     done
 }
 
-
-#######################################
-# Given a string, continuously prints the string with an 
-# updated spinner icon
-# Arguments:
-#   1. A string
-#######################################
-# Little spinning icon
-spin[0]="-"
-spin[1]="\\"
-spin[2]="|"
-spin[3]="/"
-function printf_with_spinner {
-    local string=$1
-    local width="$(tput cols)"
-    local string_length=${#string}
-    local remainder=$(($width - $string_length - 1))
-    for i in "${spin[@]}"
-    do
-        # \r removes previous line
-        # %*s adds whitespaces for padding
-        # amount of padding is given as an argument, hence the * char
-        # \b$i is the spinner icon
-        # >&2 writes to stdout
-        printf "\r$1 %*s\b$i" "$remainder">&2
-        sleep 0.1
-    done
-}
-
 #######################################
 # A quick check to see if the input arguments are what you would expect
 #   only checks the amount of arguments for now
@@ -85,103 +58,81 @@ function args_precheck {
 }
 
 #######################################
+# Search through the management_dir for the scheduler file that contains the IP of the running job
+# Arguments:
+#   1: The name of the job
+#######################################
+function fetch_ip_from_management_dir {
+    local sfile="$MYBASEDIR/management_dir_$1/scheduler3.json"
+    while ! test -f $sfile; do
+        printf_with_dots "Waiting for creation of $sfile "
+    done;
+    # Search through sfile output. Grep for http://<ip>:<dask_port> and ?/token=<token> (if a token exists)
+    # Keep searching as long as it does not appear in output
+    local scheduler_address=$(cat $sfile | grep -Eo -m1 "tcp://[0-9]{2,3}\.[0-9]+\.[0-9]+\.[0-9]+:$DASK_PORT" )
+    while [ -z "$scheduler_address" ]; do  # wait until link is written and grep returns a match
+        printf_with_dots "Launching Jupyter Lab server "
+        local scheduler_address=$(cat $sfile | grep -Eo -m1 "tcp://[0-9]{2,3}\.[0-9]+\.[0-9]+\.[0-9]+:$DASK_PORT" )
+    done;
+    # remove prefix and suffix to get just the ip
+    ip=${scheduler_address#"tcp://"}
+    ip=${ip%":$DASK_PORT"}
+    echo "${ip}"
+}
+
+
+#######################################
 # Search through the management_dir jupyter.txt file for the jupyter link (including token if it is configured to use it)
 #   greps for 11113 (the default port for jupyter lab) and outputs the first match.
 # Arguments:
 #   1: The name of the job
+#   2: Which port to search for (11112 for notebook, 11113 for lab)
 #######################################
-function fetch_jupyter_lab_link {
+function fetch_jupyter_link_from_file {
     local jupyter_file="$MYBASEDIR/management_dir_$1/jupyter.txt"
     while ! test -f $jupyter_file; do
         printf_with_dots "Setting up jupyter lab server "
     done;
-    local link="$(cat $jupyter_file | grep -Eo "LabApp] http://.*" | grep -Eo http://.* | head -1)"
+    # Search through jupyter output. Grep for http://<ip>:<port> and ?/token=<token> (if a token exists)
+    # Keep searching as long as it does not appear in output
+    local link=$(cat $jupyter_file | grep -Eo -m1 "http://[0-9]{2,3}\.[0-9]+\.[0-9]+\.[0-9]+:$2[/?token=[a-z0-9]*]?" )
     while [ -z "$link" ]; do  # wait until link is written and grep returns a match
         printf_with_dots "Launching Jupyter Lab server "
-        local link="$(cat $jupyter_file | grep -Eo "LabApp] http://.*" | grep -Eo http://.* | head -1)"
+        local link=$(cat $jupyter_file | grep -Eo -m1 "http://[0-9]{2,3}\.[0-9]+\.[0-9]+\.[0-9]+:$2[/?token=[a-z0-9]*]?" )
     done;
     echo "${link}"
 }
 
 #######################################
-# Search through the management_dir jupyter.txt file for the jupyter link (including token if it is configured to use it)
-#   greps for 11112 (the default port for jupyter lab) and outputs the first match.
+# Jupyter outputs the link with an IP different from the one we want to use.
+# This function replaces the IP with the correct one.
 # Arguments:
 #   1: The name of the job
+#   2: The jupyter link with wrong IP
+#   3: The port of the jupyter server (11112 for notebook, 11113 for lab)
 #######################################
-function fetch_jupyter_notebook_link {
-    local jupyter_file="$MYBASEDIR/management_dir_$1/jupyter.txt"
-    while ! test -f $jupyter_file; do
-        printf_with_dots "Setting up Jupyter Notebook server "
-    done;
-    local link="$(cat $jupyter_file | grep -Eo "NotebookApp] http://.*" | grep -Eo http://.* | head -1)"
-    while [ -z "$link" ]; do  # wait until link is written and grep returns a match
-        printf_with_dots "Launching Jupyter Notebook server "
-        local link="$(cat $jupyter_file | grep -Eo NotebookApp] http://.* | grep -Eo http://.* | head -1)"
-    done;
-    echo "${link}"
+function format_jupyter_links {
+    local ip=$(fetch_ip_from_management_dir $1)
+    local token=$(echo $2 | grep -Eo "(/\?token=[a-z0-9]*)?")
+    echo http://$ip:$3$token
 }
 
-
-#######################################
-# Search through the management_dir scheduler.json file for the IP of the node
-#   greps for tcp:// and 28786 (the default port for python 2 scheduler) and extracts the ip from inbetween
-# Arguments:
-#   1: The name of the job
-#######################################
-function fetch_ip {
-    management_dir="$MYBASEDIR/management_dir_$1"
-    # management dir does not eexist yet. Assume it is still being created.
-    while ! test -d "$management_dir"; do
-        printf_with_dots "Creating \"management_dir_$1\" "
-    done;
-    while ! test -f "$management_dir/scheduler.json"; do
-        printf_with_dots "Creating \"management_dir_$1/scheduler.json\" "
-    done;
-    # grep for tcp://something:dask_port, and then grep for anything inbetween tcp:// and :
-    local ip="$(cat $management_dir/scheduler.json | grep -Eo tcp://\.*:$DASK_PORT | grep -o -P '(?<=tcp://).*(?=:)')"
-    echo "${ip}"
-}
-
-#######################################
-# Construct the Jupter server link based on the node IP adress and the jupyter link
-#   Should work whether or not you have a token configured (only tested on token-having links)
-#   Makes sure the link is set up with an IP, and not the node name (useful for e.g. quickly checking dask interface)
-# Arguments:
-#   1: The IP of the job
-#   2: The jupyter link
-#######################################
-function format_jupyter_link {
-    # In the jupyter link, grep for everything after "?token="
-    local token="$(echo $2 | grep -o -P '(?<=\/\?token\=).*')"
-    local port_number="$(cut -d':' -f3 <<<$2 | cut -c1-5)"  # cut the ip on colon take third element, take first 4 chars of that
-    if [ -z "$token" ] ;  # no suffix is found: no token
-    then
-        printf "\nWarning: No token is set for this Jupyter server.\nVSCode will not be able to connect to this server, and notebooks running on this server can not be shared by link.\n">&2
-    fi
-    local jupyter_link="http://$1:$port_number/?token=$token"
-    echo $jupyter_link
-}
-
-
+# Main
 args_precheck $#;  # check amount of arguments
 job_name="$1"
 jupyter_file="$MYBASEDIR/management_dir_$job_name/jupyter.txt"
-ip="$(fetch_ip $job_name)";
-notebook_link="$(fetch_jupyter_notebook_link $job_name)";
-jupyter_notebook_link="$(format_jupyter_link $ip $notebook_link)";
-lab_link="$(fetch_jupyter_lab_link $job_name)";
-jupyter_lab_link="$(format_jupyter_link $ip $lab_link)";
+notebook_link="$(fetch_jupyter_link_from_file $job_name $NOTEBOOK_PORT)";
+lab_link="$(fetch_jupyter_link_from_file $job_name $LAB_PORT)";
+# insert IP of correct node in jupyter link
+notebook_link=$(format_jupyter_links $job_name $notebook_link $NOTEBOOK_PORT);
+lab_link=$(format_jupyter_links $job_name $lab_link $LAB_PORT);
 
 width="$(tput cols)"
 printf '\r%*s' $width  # clear previous line
 printf "${ORANGE}Jupyter Lab${NC} server for \"$1\" is running at:
-$jupyter_lab_link
+$lab_link
 
 ${ORANGE}Jupyter notebook${NC} server for \"$1\" is running at:
-$jupyter_notebook_link
+$notebook_link
 \n"
 exit 0;
-
-
-# TODO: if the management_dir exists, this script should work. QOS checks should happen in submit, not here
