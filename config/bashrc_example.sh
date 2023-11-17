336f7fe9394318c82796a7c91be8a0041f1019be
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-11-17 10:50:47 +0100

    rename

--- config/bashrc_example.sh
+++ config/bashrc_example.sh
@@ -6,7 +6,7 @@ export PYTHONPATH=$MYBASEDIR/project_src/in_silico_framework
 export ISF_HOME=$MYBASEDIR/project_src/in_silico_framework
 alias source_isf='source $MYBASEDIR/anaconda_isf2.7/bin/activate; export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH; cd $MYBASEDIR'
 alias source_3='source $MYBASEDIR/anaconda_isf3.8/bin/activate; export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH; cd $MYBASEDIR'
-alias source_33='source $MYBASEDIR/anaconda_isf3.9/bin/activate; export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH; cd $MYBASEDIR'
+alias source_39='source $MYBASEDIR/anaconda_isf3.9/bin/activate; export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH; cd $MYBASEDIR'
 alias isf='cd $ISF_HOME'
 alias wip='cd $MYBASEDIR/notebooks'
 alias data='cd $MYBASEDIR'
