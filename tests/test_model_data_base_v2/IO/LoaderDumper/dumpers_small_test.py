6ec6a5d0c14d3cadf763cab07b9075d83c339946
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-11-22 12:46:44 +0100

    added separate tests for mdbv2

---
+++ tests/test_model_data_base_v2/IO/LoaderDumper/dumpers_small_test.py
@@ -0,0 +1,131 @@
+import numpy as np
+from pandas.util.testing import assert_frame_equal
+import dask 
+from model_data_base.IO.LoaderDumper import dask_to_csv, numpy_to_npy, pandas_to_parquet, dask_to_parquet, \
+                                pandas_to_msgpack, to_pickle, pandas_to_pickle, dask_to_msgpack, \
+                                dask_to_categorized_msgpack, to_cloudpickle, reduced_lda_model
+from tests.test_simrun2.reduced_model.get_kernel_test import get_test_Rm
+from numpy.testing import assert_array_equal
+from model_data_base.utils import df_colnames_to_str
+import pytest
+import six
+
+def robust_del_fun(mdb, key):
+    try:
+        del mdb[key]
+    except KeyError:
+        pass
+
+
+def clean_up(mdb):
+    robust_del_fun(mdb, 'synapse_activation2')
+    robust_del_fun(mdb, 'voltage_traces2')
+    robust_del_fun(mdb, 'test')
+
+
+def data_frame_generic_small(mdb, pdf, ddf, dumper, client=None):
+    """
+    This function provides a generic way to test dumpers for dataframes.
+
+    Args:
+        mdb (ModelDataBase): An instance of the ModelDataBase class.
+        pdf (pd.DataFrame): A pandas DataFrame.
+        ddf (dask.DataFrame): A dask DataFrame.
+        dumper (string or dumper): A string or dumper object.
+        client (distrubuted.Client, optional): A distributed client object. Defaults to None.
+    """
+    #index not set
+    clean_up(mdb)
+    if client is None:
+        mdb.setitem('test', ddf, dumper=dumper)
+    else:
+        mdb.setitem('test', ddf, dumper=dumper, client=client)
+    dummy = mdb['test']
+    a = dask.compute(dummy)[0].reset_index(drop=True)
+    b = pdf.reset_index(drop=True)
+    if dumper in (pandas_to_parquet, dask_to_parquet):
+        b = df_colnames_to_str(b)
+    assert_frame_equal(a, b)
+    
+    #sorted index set
+    clean_up(mdb)
+    if client is None:
+        mdb.setitem('test', ddf.set_index(0), dumper=dumper)
+    else:
+        mdb.setitem('test', ddf.set_index(0), dumper=dumper, client=client)
+    dummy = mdb['test']
+    a = dask.compute(dummy)[0]
+    b = pdf.set_index(0)
+    if dumper in (pandas_to_parquet, dask_to_parquet):
+        b = df_colnames_to_str(b)
+        b.index.name = str(b.index.name)
+    assert_frame_equal(a, b)
+
+def test_dask_to_csv_small(fresh_mdb_v2, pdf, ddf):
+    data_frame_generic_small(fresh_mdb_v2, pdf, ddf, dask_to_csv)
+
+def test_dask_to_msgpack_small(fresh_mdb_v2, pdf, ddf, client):
+    data_frame_generic_small(fresh_mdb_v2, pdf, ddf, dask_to_msgpack,
+        client=client)
+
+def test_dask_to_categorized_msgpack_small(fresh_mdb_v2, pdf, ddf, client):
+    data_frame_generic_small(fresh_mdb_v2, pdf, ddf, dask_to_categorized_msgpack,
+        client=client)
+
+def test_pandas_to_msgpack_small(fresh_mdb_v2, pdf):
+    data_frame_generic_small(fresh_mdb_v2, pdf, pdf.copy(), pandas_to_msgpack)
+
+@pytest.mark.skipif(six.PY2, reason="Pandas DataFrames objects have no attribute `to_parquet` in Python 2.")
+def test_pandas_to_parquet_small(fresh_mdb_v2, pdf):
+    data_frame_generic_small(fresh_mdb_v2, pdf, pdf.copy(), pandas_to_parquet)
+
+@pytest.mark.skipif(six.PY2, reason="Pandas DataFrames objects have no attribute `to_parquet` in Python 2.")
+def test_dask_to_parquet_small(fresh_mdb_v2, pdf, ddf, client):
+    data_frame_generic_small(fresh_mdb_v2, pdf, ddf, dask_to_parquet, client=client)
+
+def test_pandas_to_pickle_small(fresh_mdb_v2, pdf):
+    data_frame_generic_small(fresh_mdb_v2, pdf, pdf.copy(), pandas_to_pickle)
+
+def test_to_pickle_small(fresh_mdb_v2, pdf):
+    data_frame_generic_small(fresh_mdb_v2, pdf, pdf.copy(), to_pickle)
+
+def test_to_cloudpickle_small(fresh_mdb_v2, pdf):
+    data_frame_generic_small(fresh_mdb_v2, pdf, pdf.copy(), to_cloudpickle)
+
+def test_self_small(fresh_mdb_v2, pdf):
+    data_frame_generic_small(fresh_mdb_v2, pdf, pdf.copy(), 'self')
+
+def test_numpy_to_npy(fresh_mdb_v2, pdf):
+
+    def fun(x):
+        clean_up(fresh_mdb_v2)
+        fresh_mdb_v2.setitem('test', x, dumper=numpy_to_npy)
+        dummy = fresh_mdb_v2['test']
+        assert_array_equal(dummy, x)
+
+    fun(np.random.randint(5, size=(100, 100)))
+    fun(np.random.randint(5, size=(100,)))
+    fun(np.array([]))
+
+def test_reduced_lda_model(fresh_mdb_v2):
+        Rm = get_test_Rm(fresh_mdb_v2)
+        # does not change the original object
+        st = Rm.st
+        lda_values = Rm.lda_values
+        lda_value_dicts = Rm.lda_value_dicts
+        mdb_list = Rm.mdb_list
+
+        fresh_mdb_v2.setitem('rm', Rm, dumper=reduced_lda_model)
+
+        assert st is Rm.st
+        assert lda_values is Rm.lda_values
+        assert lda_value_dicts is Rm.lda_value_dicts
+        assert mdb_list is Rm.mdb_list
+
+        # can be loaded
+        Rm_reloaded = fresh_mdb_v2['rm']
+
+        # is functional
+        Rm_reloaded.plot()
+        fresh_mdb_v2.setitem('rm2', Rm_reloaded, dumper=reduced_lda_model)
+        Rm_reloaded.get_lookup_series_for_different_refractory_period(10)
---
+++ tests/test_model_data_base_v2/context.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+import os, sys, shutil, tempfile
+import distributed
+import pytest
+
+parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, parent)
+import distributed
+from model_data_base import *
+import getting_started
+import mechanisms
+# set up paths
+test_data_folder = os.path.join(getting_started.parent, \
+                              'example_simulation_data', \
+                              'C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/')
+
+# cell_param_path = os.path.join(parent, 'test_model_data_base/data/test_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/20150815-1530_20240/20240_neuron_model.param')
+# assert os.path.exists(cell_param_path)
+# network_param_path = os.path.join(parent, 'test_model_data_base/data/test_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/20150815-1530_20240/20240_network_model.param')
+# assert os.path.exists(network_param_path)
+# test_data_folder = os.path.join(parent, 'test_model_data_base/data/test_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/')
+
+
+class FreshlyInitializedMdb(object):
+    '''context manager that provides a freshly initalized mdb for 
+    testing purposes
+
+    It is recommended to use the pytest fixture fresh_mdb_v2 instead of this class, except when the location of the mdb is of importance. This is the case in tests:
+    - tests/test_model_data_base/Model_data_base_test.py::test_compare_old_mdb_with_freshly_initialized_one
+    '''
+
+    def __init__(self, *args, **kwargs):
+        self.path = None
+        self.mdb = None
+
+    def __call__(self, client):
+        self.path = tempfile.mkdtemp()
+        self.mdb = model_data_base.ModelDataBase(self.path)
+        #self.mdb.settings.show_computation_progress = False
+        from model_data_base.mdb_initializers.load_simrun_general import init
+        from model_data_base.utils import silence_stdout
+        with silence_stdout:
+            init(self.mdb,
+                 test_data_folder,
+                 client=client,
+                 rewrite_in_optimized_format=False,
+                 parameterfiles=False,
+                 dendritic_voltage_traces=False)
+
+    def __enter__(self):
+        return self.mdb
+
+    def __exit__(self, *args, **kwargs):
+        if os.path.exists(self.path):
+            shutil.rmtree(self.path)
