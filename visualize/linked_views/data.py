7793d1efea2636a69bb7b694fdbd1188a91bfc98
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-12-07 16:00:28 +0100

    Abstract Dataclasses: added pandas wrapper TODO: compute selection

---
+++ visualize/linked_views/data.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from collections.abc import Sequence
+
+class PandasTableWrapper:
+    def __init__(self, data):
+        self.df = data
+        self.name = "Pandas DataFrame"
+        self.shape = self.df.shape
+        self.columns = self.df.columns
+
+    def binby(self, columns, binsize, mode="mean", value_col=None):
+        assert len(columns) == 2, "Please provide an array of 2 columns to bin by. You provided {}".format(columns)
+        assert (value_col == None and mode=="count") or (value_col != None and mode != "count"), "If mode = count, you may not pass a value_col, otherwise you must."
+        c1, c2 = columns
+        if mode == "count":
+            return self.df.groupby([(selfdf[c1]/binsize).round()*binsize, 
+                    (self.df[c2]/binsize).round()*binsize]).apply(lambda x: x.count())
+        else:
+            agg = aggregation_mode_mapping[mode] if isinstance(mode, str) else mode
+            return self.df.groupby([(selfdf[c1]/binsize).round()*binsize, 
+                    (self.df[c2]/binsize).round()*binsize]).apply(lambda x: agg(x[value_col]))
+
+    def count(self, binby=None, shape=None, selection=None, limits=None):
+        binsize = get_binsize(shape, limits)
+        assert not isinstance(binsize, Sequence), "Nested binning/shape/limits are not supported."
+        if all([x is None for x in [binby, shape, selection, limits]]):
+            return self.df.count()
+        return self.df.iloc[selection].clip(*limits).binby(columns=binby, binsize=binsize, mode="count")
+
+    def min(self, column=None, binby=None, shape=None, selection=None, limits=None):
+        if all([x is None for x in [binby, shape, selection, limits]]):
+            return self.df.min()
+        binsize = get_binsize(shape, limits)
+        assert not isinstance(binsize, Sequence), "Nested binning/shape/limits are not supported."
+        return self.df.iloc[selection].clip(*limits).binby(columns=binby, binsize=binsize, mode="min")
+
+    def max(self, column=None, binby=None, shape=None, selection=None, limits=None):
+        if all([x is None for x in [binby, shape, selection, limits]]):
+            return self.df.max()
+        binsize = get_binsize(shape, limits)
+        assert not isinstance(binsize, Sequence), "Nested binning/shape/limits are not supported."
+        return self.df.iloc[selection].clip(*limits).binby(columns=binby, binsize=binsize, mode="max")
+
+    def mean(self, column=None, binby=None, shape=None, selection=None, limits=None):
+        if all([x is None for x in [binby, shape, selection, limits]]):
+            return self.df.mean()
+        binsize = get_binsize(shape, limits)
+        assert not isinstance(binsize, Sequence), "Nested binning/shape/limits are not supported."
+        return self.df.iloc[selection].clip(*limits).binby(columns=binby, binsize=binsize, mode="mean")
+
+    def median(self, column=None, binby=None, shape=None, selection=None, limits=None):
+        if all([x is None for x in [binby, shape, selection, limits]]):
+            return self.df.median()
+        binsize = get_binsize(shape, limits)
+        assert not isinstance(binsize, Sequence), "Nested binning/shape/limits are not supported."
+        return self.df.iloc[selection].clip(*limits).binby(columns=binby, binsize=binsize, mode="median")
+
+    def compute_selection(self, ):
+        # TODO
+        pass
+
+aggregation_mode_mapping = {
+    'mean': lambda x: x.mean(),
+    'median': lambda x: x.median(),
+    'min': lambda x: x.min(),
+    'max': lambda x: x.max(),
+}
+
+def get_binsize_1d(shape, limits):
+    binsize = (limits[1] - limits[0])/shape
+    return binsize
+
+def get_binsize(shape, limits):
+    assert isinstance(shape, Sequence) or isinstance(shape, int), "shape should be an integer or array like"
+    if isinstance(shape, int) and isinstance(limits[0], Sequence):
+        # limits is nested array implying shape should be array, but shape is scalar
+        # vaex supports passing single integer for all columns
+        shape = [shape]*len(limits)
+    
+    if type(shape) == int:
+            return get_binsize_1d(shape, limits)
+    else:
+        assert len(shape) == len(limits), "Got {} values for shape, but {} limits".format(len(shape, len(limits)))
+        assert all([len(limit_pair) == 2 for limit_pair in limits]), "All elements in limits should be  pair of values"
+        return [get_binsize_1d(s, l) for s, l in zip(shape, limits)]
--- visualize/linked_views/server.py
+++ visualize/linked_views/server.py
@@ -5,14 +5,14 @@ import logging
 from logging import handlers
 import tempfile
 import threading
-import util
+from . import util
 import glob
 import json
 import pandas as pd
 import numpy as np
 import vaex
 import vaex.ml
-
+from .data import PandasTableWrapper
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
@@ -60,6 +60,15 @@ def load_table_from_df(df):
     tables[basename]["records"] = columns_data
     return tables
 
+def load_table_from_adf(adf):
+    tables = {}
+    basename = "pandas_df"
+    tables[basename] = {}
+    tables[basename]["flat"] = adf
+    columns_data = adf.df.to_dict(orient="records")
+    tables[basename]["records"] = columns_data
+    return tables
+
 
 def write_objects_to_file(filenameProjects, objects):    
     if(filenameProjects is None):
@@ -67,10 +76,17 @@ def write_objects_to_file(filenameProjects, objects):
     with open(filenameProjects, 'w') as file:
         json.dump(objects, file)
 
-def get_data_ranges(df):
+def get_data_ranges(adf):
+    """
+    Given an abstract dataframe, return the data ranges for each column.
+
+    Args:
+        adf: data.AbstractDataFrame
+    """
+    col_names = adf.columns
     ranges = {}
-    ranges["min"] = df.min().to_list()
-    ranges["max"] = df.max().to_list()
+    ranges["min"] = adf.min().to_list()
+    ranges["max"] = adf.max().to_list()
     return ranges 
 
 def get_data_ranges_vaex(df):
@@ -100,23 +116,24 @@ def getPCA(df):
 
     
 class LinkedViewsServer:
-    def __init__(self, config_file_path = None, data_folder=None):
+    def __init__(self, config_file_path = None, data_folder=None, log_dir=None):
         if(config_file_path is not None):
             self.config_file_path = config_file_path
         else:
             self.config_file_path = Path(os.path.dirname(__file__))/"defaults"/"config.json"        
 
         self.data_folder = None 
-        if(data_folder):
+        if data_folder:
             self.data_folder = Path(data_folder)
             self.resourceDir = self.data_folder/"resources"
             self.config_file_path = self.data_folder/"config.json"
         else:
             self.resourceDir = None
         self.filenameProjects = None
+        self.log_dir = log_dir
 
         self.thread = None    
-        self.vaex_df = None
+        self.abstract_df = None
         self.selections = {}
         self.active_selection = None
 
@@ -175,7 +192,7 @@ class LinkedViewsServer:
         print("Server stopped.")
 
     def start_logging(self):
-        tmp_folder = Path(tempfile.gettempdir())
+        tmp_folder = Path(self.log_dir)/"tmp"  # TODO for soma_fs
         log_filename = tmp_folder/"linked-views-server.log"
         print("logs are written to {}".format(log_filename))
         self.logfile_handler = logging.handlers.RotatingFileHandler(
@@ -195,18 +212,17 @@ class LinkedViewsServer:
         assert self.server is not None
 
         if(isinstance(df, pd.DataFrame)):
-            self.tables = load_table_from_df(df)
-            self.config["cached_tables"] = ["pandas_df"]
+            self.abstract_df = PandasTableWrapper(df)
+            self.config["cached_tables"] = ["PandasTableWrapper"]
         elif(isinstance(df, vaex.DataFrame)):
-            self.vaex_df = df        
-            self.vaex_columns = df.get_column_names()         
-            self.vaex_df["row_index"] = np.arange(self.vaex_df.shape[0])
-            self.vaex_data_ranges = get_data_ranges_vaex(df)
+            self.abstract_df = df        
+            self.abstract_df["row_index"] = np.arange(self.abstract_df.shape[0])
+            self.config["cached_tables"] = ["Vaex DataFrame"]
         else:
             raise TypeError(df)
-
-        
-    
+        self.tables = load_table_from_adf(self.abstract_df)
+        self.columns = self.abstract_df.get_column_names()
+        self.data_ranges = get_data_ranges(self.abstract_df)
 
     def init_routes(self):
         self.app.add_url_rule('/', 'index', self.index)
@@ -223,8 +239,8 @@ class LinkedViewsServer:
         self.app.add_url_rule("/dataServer/setIndicesSelection", "setIndicesSelection", self.setIndicesSelection, methods=["POST"])
 
     def index(self):
-        if(self.vaex_df):
-            return "vaex df columns: {}".format([name for name in list(self.vaex_df.columns)])
+        if(self.abstract_df):
+            return "vaex df columns: {}".format([name for name in list(self.abstract_df.columns)])
         else:
             return "Tables: {}".format([name for name in self.tables.keys()])
 
@@ -292,21 +308,21 @@ class LinkedViewsServer:
                 
                 meta_data = []            
                 for tableName, tableData in self.tables.items():
-                    df = tableData["flat"]            
+                    adf = tableData["flat"]            
                     meta_data.append({
                         "name" : tableName,
-                        "num_rows" : df.shape[0],
-                        "columns" : df.columns.to_list(),
-                        "data_ranges" : get_data_ranges(df)                
+                        "num_rows" : adf.shape[0],
+                        "columns" : adf.columns,
+                        "data_ranges" : get_data_ranges(adf)                
                     })
 
-                if(self.vaex_df is not None):                       
-                    meta_data.append({
-                        "name" : "vaex_df",
-                        "num_rows" : self.vaex_df.shape[0],
-                        "columns" : self.vaex_columns,
-                        "data_ranges" : self.vaex_data_ranges#'get_data_ranges_vaex(self.vaex_df)                
-                    })
+                # if(self.abstract_df is not None):                       
+                #     meta_data.append({
+                #         "name" : "vaex_df",
+                #         "num_rows" : self.abstract_df.shape[0],
+                #         "columns" : self.columns,
+                #         "data_ranges" : self.data_ranges#'get_data_ranges_vaex(self.abstract_df)                
+                #     })
 
 
                 response_data = {
@@ -352,20 +368,21 @@ class LinkedViewsServer:
                 data = request.get_json(force=True)
 
                 tableName = data["table"]
-                df = self.tables[tableName]["flat"]
+                adf = self.tables[tableName]["flat"]
 
                 columns = data["columns"]
                 indices = data["indices"]
                 format = data["format"]
                 #print(columns)
                 #print(set(columns) - set(df.columns))
-                assert set(columns).issubset(set(df.columns))
-                assert len(indices) == 0 or max(indices) < df.shape[0]            
+                assert set(columns).issubset(set(adf.columns))
+                assert len(indices) == 0 or max(indices) < adf.shape[0]         
 
+                
                 if(len(indices) == 0):
-                    filtered_df = df[columns]
+                    filtered_df = adf.df[columns]
                 else:
-                    filtered_df = df.iloc[indices][columns]
+                    filtered_df = adf.df.iloc[indices][columns]
 
                 if(format == "expanded"):
                     values = filtered_df.to_dict(orient="records")
@@ -398,7 +415,7 @@ class LinkedViewsServer:
                 if(tableName not in ["vaex_df"]):
                     raise ValueError(tableName)
 
-                df = self.vaex_df
+                df = self.abstract_df
               
                 columns = data["columns"]                
                 #indices = data["indices"]
@@ -413,6 +430,7 @@ class LinkedViewsServer:
 
                 binbycols = columns[0:2]
                 density_grid_shape = tuple(data["density_grid_shape"])
+                print("density_grid_shape", density_grid_shape)
                 nCells = density_grid_shape[0] * density_grid_shape[1]
                 #indices = np.arange(nCells)
                                                 
@@ -421,18 +439,33 @@ class LinkedViewsServer:
                 data_ranges = [minmax_x.tolist(), minmax_y.tolist()]
                 
                 if(format == "count"):
-                    values = df.count(binby=binbycols, shape=density_grid_shape, selection=self.active_selection, limits=data_ranges).astype(float)
+                    values = df.count(
+                        binby=binbycols, 
+                        shape=density_grid_shape, selection=self.active_selection, 
+                        limits=data_ranges).astype(float)
                     values[values == 0] = np.nan
                 elif(format == "min"):                    
-                    values = df.min(value_column, binby=binbycols, shape=density_grid_shape, selection=self.active_selection, limits=data_ranges)
+                    values = df.min(
+                        value_column, binby=binbycols, 
+                        shape=density_grid_shape, selection=self.active_selection, 
+                        limits=data_ranges)
                     values[values > 10**100] = np.nan
                 elif(format == "max"):                    
-                    values = df.max(value_column, binby=binbycols, shape=density_grid_shape, selection=self.active_selection, limits=data_ranges)
+                    values = df.max(
+                        value_column, binby=binbycols, 
+                        shape=density_grid_shape, selection=self.active_selection, 
+                        limits=data_ranges)
                     values[values < -10**100] = np.nan
                 elif(format == "mean"):                    
-                    values = df.mean(value_column, binby=binbycols, shape=density_grid_shape, selection=self.active_selection, limits=data_ranges)
+                    values = df.mean(
+                        value_column, binby=binbycols, 
+                        shape=density_grid_shape, selection=self.active_selection,
+                        limits=data_ranges)
                 elif(format == "median"):
-                    values = df.median_approx(value_column, binby=binbycols, shape=density_grid_shape, selection=self.active_selection, limits=data_ranges)
+                    values = df.median_approx(
+                        value_column, binby=binbycols, 
+                        shape=density_grid_shape, selection=self.active_selection, 
+                        limits=data_ranges)
                 else:
                     raise ValueError(format)             
 
@@ -463,7 +496,7 @@ class LinkedViewsServer:
                 
                 assert table == "vaex_df"
                 for column in columns:
-                    assert column in self.vaex_columns
+                    assert column in self.columns
 
                 self.selections[selection_name] = {
                     "columns" : columns,
@@ -500,14 +533,14 @@ class LinkedViewsServer:
 
             ranges = self.selections["global"]["bin_ranges"]
 
-            self.vaex_df.select_nothing(name="selection_global")
+            self.abstract_df.select_nothing(name="selection_global")
             for idx, range_i in enumerate(ranges):    
                 limit_i = [(range_i[0][0], range_i[0][1]), (range_i[1][0], range_i[1][1])]        
-                self.vaex_df.select_rectangle(self.vaex_df[col1], self.vaex_df[col2], limit_i, name="selection_global", mode="or")
+                self.abstract_df.select_rectangle(self.abstract_df[col1], self.abstract_df[col2], limit_i, name="selection_global", mode="or")
             self.active_selection = "selection_global"
 
             if(return_indices):
-                df_selected_indices = self.vaex_df.evaluate(self.vaex_df["row_index"], selection="selection_global")
+                df_selected_indices = self.abstract_df.evaluate(self.abstract_df["row_index"], selection="selection_global")
                 return df_selected_indices
             else:
                 return "selection_global"
