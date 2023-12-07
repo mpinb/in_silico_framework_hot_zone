5d37db4da2f5b3ac4ec7092858b5880e0328a996
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2023-12-07 15:59:10 +0100

    minor sync with master

--- SLURM_scripts/setup_jupyter_server.py
+++ SLURM_scripts/setup_jupyter_server.py
@@ -1,11 +1,11 @@
 import os
-from setup_locking_server import check_locking_config
+from SLURM_scripts.setup_locking_server import check_locking_config
 
 
 ##############################################
 # setting up jupyter-notebook
 #############################################
-def setup_jupyter_server(management_dir, ports):
+def setup_jupyter_server(management_dir, ports, token=""):
     """Sets up the jupyter server for both jupyter notebook and jupyter lab
     This process is normally exectuted by only one thread on the cluster.
 
@@ -14,14 +14,16 @@ def setup_jupyter_server(management_dir, ports):
         ports (dict | dict-like): A dictionary of port numbers to use for the dask setup.
             Must containg the following keys: 'dask_client_2', 'dask_dashboard_2', 'dask_client_3' and 'dask_dashboard_3'
             Each key must have a port number as value.
-            Should be specified in ./user_settings.ini
+            Should be specified in config/user_settings.ini
     """
     print('-' * 50)
     print('setting up jupyter notebook')
     check_locking_config()
 
     ##### Setup server for Jupyter notebook #####
-    command = "cd notebooks; jupyter-notebook --ip='*' --no-browser --port={} "
+    command = '''cd notebooks; jupyter-notebook --ip='*' --no-browser --port={} '''
+    if token != "":
+        command += "--NotebookApp.token='{}' ".format(token)
     command = command.format(ports['jupyter_notebook'])
     print(command)
     # Redirect both stdout and stderr (&) to file
@@ -30,16 +32,22 @@ def setup_jupyter_server(management_dir, ports):
     print('-' * 50)
 
     ##### Setup server for Jupyter Lab #####
+    print("Setting up JupyterLab")
     #command = "conda activate /axon/scratch/abast/anaconda3/; jupyter-lab --ip='*' --no-browser --port=11113 &"
     #command = 'screen -S jupyterlab -dm bash -c "source ~/.bashrc; source_3; ' +     '''jupyter-lab --ip='*' --no-browser --port=11113"'''
     #command = '''bash -c "source ~/.bashrc; source_3; jupyter-lab --ip='*' --no-browser --port=11113" &'''
     #command = '''(source ~/.bashrc; source_3; jupyter-lab --ip='*' --no-browser --port=11113) &'''
-    command = '''bash -ci "source ~/.bashrc; source_3; jupyter-lab --ip='*' --no-browser --port={} --NotebookApp.allow_origin='*' " '''
-    command = command.format(ports['jupyter_lab'])
+    command = '''source ~/.bashrc; source_3; jupyter-lab --ip='*' --no-browser --port={} --NotebookApp.allow_origin='*' '''.format(ports['jupyter_lab'])
+    if token != "":
+        command += "--NotebookApp.token='{}' ".format(token)
+    # run comand with bash -ci
+    command = "bash -ci \"{}\"".format(command)
+    print(command)
     #command = "/axon/scratch/abast/anaconda3/bin/jupyter-lab --ip='*' --no-browser --port=11113"
     # append output to same file as notebook (ance the >> operator rather than >)
     os.system(command +
               "&>>{} &".format(os.path.join(management_dir, "jupyter.txt")))
+    print('-' * 50)
 
 
 if __name__ == "__main__":
