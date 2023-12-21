[bugfix] moved masking values to backend

visualize/linked_views/data.py
@@ -236,7 +236,7 @@ def get_binsize(shape, limits):
         assert all([len(limit_pair) == 2 for limit_pair in limits]), "All elements in limits should be  pair of values"
         return [get_binsize_1d(s, l) for s, l in zip(shape, limits)]
 
-def mask_invalid_values(values, operation, invalid_value):
+def mask_invalid_values(values, operation, mask_value):
     """Given an array of values, masks invalid values as specified by the invalid_values argument.
     What constitutes an invalid value depends on the operation being performed.
 
@@ -246,13 +246,14 @@ def mask_invalid_values(values, operation, invalid_value):
         invalid_value (float | int): Value to use as masking value
     """
     if operation == "count":
-        values = values[values == 0] = invalid_value
+        values[values == 0] = mask_value
     elif operation=='max':
-        values = values[values < -10**100] = invalid_value
+        values[values < -10**100] = mask_value
     elif operation=='min':
-        values = values[values > 10**100] = invalid_value
+        values[values > 10**100] = mask_value
     elif operation in ['mean', 'median']:
         pass
     else:
         raise ValueError(f"Invalid operation. Choose from ['mean', 'median', 'min', 'max', 'count'].")
+    values[np.isnan(values)] = mask_value
     return values

visualize/linked_views/server.py
@@ -441,16 +441,16 @@ class LinkedViewsServer:
         self.last_request = request.data
         if request.method == "POST":            
             if request.data:
-                request_data = request.get_json(force=True)
+                data = request.get_json(force=True)
 
-                assert request_data["table"] == "Abstract DataFrame", \
+                assert data["table"] == "Abstract DataFrame", \
                     "Only Abstract DataFrames are supported. \
                     If you are not using pandas or vaex tables, please create a wrapper in data.py."
 
                 adf = self.abstract_df
-                columns = request_data["columns"]                
+                columns = data["columns"]                
                 # indices = data["indices"]
-                agg_format = request_data["format"]
+                agg_format = data["format"]
                 if agg_format in ["count"]:
                     assert len(columns) == 2
                     value_column=None
@@ -461,7 +461,7 @@ class LinkedViewsServer:
                     raise ValueError(agg_format)
 
                 binbycols = columns[0:2]
-                density_grid_shape = tuple(request_data["density_grid_shape"])
+                density_grid_shape = tuple(data["density_grid_shape"])
                 print("density_grid_shape", density_grid_shape)
                 nCells = density_grid_shape[0] * density_grid_shape[1]
                 #indices = np.arange(nCells)
@@ -473,14 +473,18 @@ class LinkedViewsServer:
                     operation=agg_format, expression = value_column,
                     binby=binbycols, shape=density_grid_shape, 
                     selection=self.active_selection,
-                    limits=data_ranges)
-                values = mask_invalid_values(values=values, operation=agg_format, mask_value=INVALID_NUMBER)        
+                    limits=data_ranges
+                    ).values
+                
+                values = mask_invalid_values(
+                    values=values, 
+                    operation=agg_format, mask_value=INVALID_NUMBER)        
                 
                 response_data = {
                     "columns" : columns,
                     #"indices" : indices.tolist(),
                     "values" : values.tolist(),
-                    "density_grid_shape" : request_data["density_grid_shape"],
+                    "density_grid_shape" : data["density_grid_shape"],
                     "data_ranges" : data_ranges,
                     "masked_value" : INVALID_NUMBER
                 }