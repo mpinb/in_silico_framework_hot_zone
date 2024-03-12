2c37d92cc382cfb2eb88122caf34bd1beebab108
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-03-12 17:14:35 +0100

    try/except on importing dash it relies in the backend on Flask, which has pretty strong dependencies. It works if you use the isf environment, but hard to break away from these

--- visualize/cell_morphology_visualizer.py
+++ visualize/cell_morphology_visualizer.py
@@ -13,12 +13,6 @@ from .utils import write_video_from_images, write_gif_from_images, display_anima
 from barrel_cortex import inhibitory
 if six.PY3:
     from scipy.spatial.transform import Rotation
-    from dash import Dash, dcc, html, Input, Output
-    from dash import callback_context as ctx
-    import plotly.offline as py
-    import plotly.io as pio
-    import plotly.graph_objects as go
-    import plotly.express as px
 else:
     # let ImportWarnings show up when importing this module through Interface
     warnings.filterwarnings("default", category=ImportWarning, module=__name__)
@@ -30,6 +24,17 @@ else:
     )
 import logging
 logger = logging.getLogger("ISF").getChild(__name__)
+# For interactive visualizations
+try:
+    from dash import Dash, dcc, html, Input, Output
+    from dash import callback_context as ctx
+    import plotly.offline as py
+    import plotly.io as pio
+    import plotly.graph_objects as go
+    import plotly.express as px
+except ImportError as e:
+    logger.warning(e)
+
 
 class CMVDataParser:
     def __init__(
