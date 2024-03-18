1738095bd1513109bc02d14a8ef134a7a667ccdb
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-03-18 18:13:16 +0100

    update data dir name

--- getting_started/__init__.py
+++ getting_started/__init__.py
@@ -2,14 +2,14 @@ from __future__ import print_function
 import os
 
 getting_started_dir = parent = os.path.abspath(os.path.dirname(__file__))
-data_dir = os.path.join(getting_started_dir, 'example_data')
+example_data_dir = os.path.join(getting_started_dir, 'example_data')
 
 
 def generate_param_files_with_valid_references():
     IN_SILICO_FRAMEWORK_DIR = os.path.abspath(
         os.path.dirname(os.path.dirname(__file__)))
     suffix = '.TEMPLATE'
-    filelist = [os.path.join(data_dir, e) for e in (
+    filelist = [os.path.join(example_data_dir, e) for e in (
                 'biophysical_constraints/86_CDK_20041214_BAC_run5_soma_Hay2013_C2center_apic_rec.param.TEMPLATE', \
                 'functional_constraints/network.param.TEMPLATE', \
                 'simulation_data/C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/20150815-1530_20240/20240_network_model.param.TEMPLATE',\
@@ -29,13 +29,13 @@ def generate_param_files_with_valid_references():
 generate_param_files_with_valid_references()
 
 hocfile = os.path.join(
-    data_dir,
+    example_data_dir,
     'anatomical_constraints/86_L5_CDK20041214_nr3L5B_dend_PC_neuron_transform_registered_C2center_scaled_diameters.hoc'
 )
-networkParam = os.path.join(data_dir,
+networkParam = os.path.join(example_data_dir,
                             'functional_constraints/network.param')
 neuronParam = os.path.join(
-    data_dir,
+    example_data_dir,
     'biophysical_constraints/86_CDK_20041214_BAC_run5_soma_Hay2013_C2center_apic_rec.param'
 )
-radiiData = os.path.join(data_dir, 'morphology',)
+radiiData = os.path.join(example_data_dir, 'morphology',)
