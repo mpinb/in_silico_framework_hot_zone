22e7555936123a2d68581fe7d6bb6e65ddfd2ec5
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-03-22 11:33:35 +0100

    update dir locations

--- tests/context.py
+++ tests/context.py
@@ -1,14 +1,15 @@
 from __future__ import absolute_import
 import os
 import sys
-import getting_started
+from getting_started import getting_started_dir
 
 parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 TEST_DATA_FOLDER = os.path.join(
-    getting_started.parent, 
+    getting_started_dir, 
     'example_data',
     'simulation_data'
     'C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/')
+assert os.path.exists(TEST_DATA_FOLDER)
 
 sys.path.insert(0, parent)
