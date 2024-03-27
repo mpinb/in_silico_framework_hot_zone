017f34c9b446f5e5540e1ee73415a28c078ee610
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-03-27 17:17:54 +0100

    update client fixture

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -6,12 +6,13 @@ def client(pytestconfig):
     # These are set up in the github workflow file.
     # If running tests locally, make sure you have a dask scheduler and dask worker running on the ports you want
     hostname = socket.gethostname()
-    if "soma" in hostname:
-        ip = socket.gethostbyname(hostname).replace('100', '102')
-    else:
-        ip = 'localhost'
+    ip = socket.gethostbyname(hostname)
+    if "soma" in hostname and not hostname[-3] == "2":
+        ip = ip.replace('100', '102')
     c = distributed.Client(
-        '{}:{}'.format(ip, pytestconfig.getoption("--dask_server_port"))
+        '{}:{}'.format(
+            ip, 
+            pytestconfig.getoption("--dask_server_port"))
         )
     def import_mechanisms(): import mechanisms.l5pt as l5pt
     c.run(import_mechanisms)
