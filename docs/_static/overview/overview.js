37d1e548b1978cd6fb31f2c700b58bd0d1de76ed
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-03-28 13:12:00 +0100

    update html

--- docs/_static/overview/overview.js
+++ docs/_static/overview/overview.js
@@ -1,11 +1,11 @@
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("organigram-collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     // Set the content style overflow of all other contents to hidden
-    var allCollapsibles = document.getElementsByClassName("collapsible");
+    var allCollapsibles = document.getElementsByClassName("organigram-collapsible");
     for (var j = 0; j < allCollapsibles.length; j++) {
       if (allCollapsibles[j] !== this) {
         var content = allCollapsibles[j].nextElementSibling;
@@ -31,44 +31,3 @@ for (i = 0; i < coll.length; i++) {
   })
 }
 
-var divA = document.querySelector("#row2");
-var arrow  = document.querySelector("#arrow");
-
-function extendConnector() {
-  var posnA = {
-    x: divA.offsetLeft + divA.offsetWidth / 3 - divA.offsetWidth/6,
-    y: divA.offsetTop 
-  };
-
-  var posnB = {
-    y: divA.offsetTop + coll[0].offsetTop
-  };
-
-  var dStr =
-      "M" +
-      (posnA.x) + "," + (posnA.y) + " " +
-      "V" +
-      (posnB.y)
-  arrow.setAttribute("d", dStr);
-};
-
-function collapseConnector() {
-  var posnA = {
-    x: divA.offsetLeft + divA.offsetWidth / 3 - divA.offsetWidth/6,
-    y: divA.offsetTop
-  };
-
-  var posnB = {
-    y: divA.children[0].offsetTop
-  };
-
-  var dStr =
-      "M" +
-      (posnA.x      ) + "," + (posnA.y) + " " +
-      "V" +
-      (posnB.y)
-  arrow.setAttribute("d", dStr);
-}
-
-
-collapseConnector();
