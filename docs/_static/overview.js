48f3b95f4f8d421a19dac50057b64379d68316d1
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-04-17 17:21:57 +0200

    update html overview

--- docs/_static/overview.js
+++ docs/_static/overview.js
@@ -1,32 +1,37 @@
-var coll = document.getElementsByClassName("organigram-collapsible");
+var buttons = document.getElementsByClassName("organigram-collapsible");
+var contents = document.getElementsByClassName("organigram-collapsible-content")
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    // Set the content style overflow of all other contents to hidden
-    var allCollapsibles = document.getElementsByClassName("organigram-collapsible");
-    for (var j = 0; j < allCollapsibles.length; j++) {
-      if (allCollapsibles[j] !== this) {
-        var content = allCollapsibles[j].nextElementSibling;
-        content.style.height = "0";
-        content.style.display = "hidden";
-      }
-    }
+    for (var j = 0; j < buttons.length; j++) {    
+        if (buttons[j] !== this) {
+            // Set the content style overflow of all other contents to hidden
+            collapseContent(j);
+          } else {
+            // Expand or collapse the content of whatever button was clicked 
+            var extended_height = "300px";  // should be an absolute number to work in all browsers
+            if (contents[j].style.height === extended_height) {
+              collapseContent(j)
+            } else {
+              expandContent(j, extended_height)
+            };
+          }
+        }
 
-    // extend or collapse current collapsible
-    var extended_height = "300px";  // should be an absolute number to work in all browsers
-    var content = this.nextElementSibling;
-    if (content.style.height === extended_height) {
-      content.style.height = "0";
-      content.style.overflow = "hidden";
-      
-    } else {
-      // expand current collapsible
-      content.style.height = extended_height;
-      content.style.display = "block";
-    };
     
   })
 }
 
+function collapseContent(j) {
+  // collapse content that corresponds to index j
+  contents[j].style.height = "0";
+  contents[j].style.display = "block";
+}
+
+function expandContent(j, height) {
+  // expand content that corresponds to index j
+  contents[j].style.height = height;
+  contents[j].style.display = "block";
+}
