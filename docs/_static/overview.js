70d5057853668d8f67884da8382f8630dea2138f
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-04-17 18:02:20 +0200

    update html

--- docs/_static/overview.js
+++ docs/_static/overview.js
@@ -1,4 +1,4 @@
-var buttons = document.getElementsByClassName("organigram-collapsible");
+var buttons = document.getElementsByClassName("organigram-button");
 var contents = document.getElementsByClassName("organigram-collapsible-content")
 var i;
 
@@ -14,8 +14,11 @@ for (i = 0; i < buttons.length; i++) {
             var extended_height = "300px";  // should be an absolute number to work in all browsers
             if (contents[j].style.height === extended_height) {
               collapseContent(j)
+              colorAllButtons();
             } else {
               expandContent(j, extended_height)
+              greyOutOtherButtons(j);
+              colorThisButton(j);
             };
           }
         }
@@ -34,4 +37,33 @@ function expandContent(j, height) {
   // expand content that corresponds to index j
   contents[j].style.height = height;
   contents[j].style.display = "block";
+}
+
+function greyOutOtherButtons(j) {
+  for (var k = 0; k < buttons.length; k++) {
+    if (k !== j) {
+      buttons[k].style.background = "grey";
+      if (buttons[k].parentNode.id === "neuron-network-row" && buttons[j].parentNode.id === "neuron-network-row") {
+        buttons[k].style.height = "80%";
+      } else {
+        buttons[k].style.height = "50px";
+      }
+    }
+  }
+}
+
+function colorThisButton(j) {
+  buttons[j].style.background = "#f29b30";
+  if (buttons[j].parentNode.id === "neuron-network-row") {
+      buttons[j].style.height = "100%";
+  }
+}
+
+function colorAllButtons() {
+  for (var k = 0; k < buttons.length; k++) {
+    buttons[k].style.background = "#f29b30";
+    if (buttons[k].parentNode.id === "neuron-network-row") {
+      buttons[k].style.height = "100%";
+    }
+  }
 }
