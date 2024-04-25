faa8f89a47be5303bb365342a770e192664e8e9f
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-04-25 14:30:19 +0200

    session-scoped clients

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,13 +1,15 @@
 import pytest, distributed
+import socket
+import six
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def client(pytestconfig):
     # Assume dask server and worker are already started
     # These are set up in the github workflow file.
     # If running tests locally, make sure you have a dask scheduler and dask worker running on the ports you want
     c = distributed.Client(
         '{}:{}'.format(
-            "localhost", 
+            socket.gethostbyname("localhost"), 
             pytestconfig.getoption("--dask_server_port"))
         )
     return c
