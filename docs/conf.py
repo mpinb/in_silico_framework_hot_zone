df2654ebba3a9d14ac5ac8aeb4698546c4d26967
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-05-07 18:01:03 +0200

    bib path was correct

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ extensions = [
     'sphinxcontrib.bibtex'  # for citations
 ]
 
-bibtex_bibfiles = ['docs/bibliography.bib']
+bibtex_bibfiles = ['bibliography.bib']
 
 ## Include Python objects as they appear in source files
 ## Default: alphabetically ('alphabetical')
