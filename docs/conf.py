aaa9bfea8727198c05aa6f030af614e295dc18d9
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-05-24 14:43:55 +0200

    explicit napoleon settings

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,21 @@ extensions = [
 
 bibtex_bibfiles = ['bibliography.bib']
 
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = True
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
 ## Include Python objects as they appear in source files
 ## Default: alphabetically ('alphabetical')
 # autodoc_member_order = 'bysource'
