e9129c52405c9fde045af7fd3b7be67aecd2d1bd
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-06-05 15:53:32 +0200

    no cluster config in isf, moved to cluster_control

--- config/bashrc_example.sh
+++ config/bashrc_example.sh
@@ -1,14 +1,6 @@
-shopt -s expand_aliases
 export MYBASEDIR="$HOME"
-module load git/2.31
-ulimit -Sn "$(ulimit -Hn)"
-export PYTHONPATH=$MYBASEDIR/project_src/in_silico_framework
 export ISF_HOME=$MYBASEDIR/project_src/in_silico_framework
 alias source_isf='source $MYBASEDIR/anaconda_isf2.7/bin/activate; export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH; cd $MYBASEDIR'
 alias source_3='source $MYBASEDIR/anaconda_isf3.8/bin/activate; export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH; cd $MYBASEDIR'
 alias source_39='source $MYBASEDIR/anaconda_isf3.9/bin/activate; export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH; cd $MYBASEDIR'
 alias isf='cd $ISF_HOME'
-alias wip='cd $MYBASEDIR/notebooks'
-alias data='cd $MYBASEDIR'
-alias cleanit='cd $MYBASEDIR; rm management_dir* -r; rm *slurm'
-export PATH=$HOME/local/bin:$PATH
