000fd3789c30a43be14f9df32bc14d7202a54925
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-06-12 12:02:46 +0200

    fig is relative to the autosummary directory

--- singlecell_input_mapper/singlecell_input_mapper/synapse_mapper.py
+++ singlecell_input_mapper/singlecell_input_mapper/synapse_mapper.py
@@ -171,9 +171,9 @@ class SynapseDensity(object):
     to compute synapse densities per presynaptic cell type for a given postsynaptic cell type and morphology.
     
     :paramref:`exPST` and :paramref:`inhPST` are density meshes for normalizing post-synaptic targets.
-    An example for the barrel cortex looks like:
+    E.g.: shown below are the (25000, 50000, 75000, 100000) isosurfaces of the excitatory PST density field for a rat somatosensory cortex.
     
-    .. figure:: docs/_static/_figures/exPST.png
+    .. figure:: ../_static/_figures/exPST.png
     '''
     def __init__(
         self, 
