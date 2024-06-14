d113e1eb423c72ccfddca1b6cf327de04ea9f6ec
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-06-14 15:10:21 +0200

    include private methods?

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ bibtex_bibfiles = ['bibliography.bib']
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
+napoleon_include_private_with_doc = True
 napoleon_include_special_with_doc = True
 napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = False
