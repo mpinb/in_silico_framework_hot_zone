cf7cf52eae6d1d1f26bf14f517602c355198e32c
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-06-14 15:41:24 +0200

    more docstring

--- singlecell_input_mapper/singlecell_input_mapper/network_embedding.py
+++ singlecell_input_mapper/singlecell_input_mapper/network_embedding.py
@@ -25,7 +25,8 @@ class NetworkMapper:
     These synapse density fields depend on the presence of post-synaptic dendrites in the bouton field,
     which in turn depends on the location and morphology of the post-syanptic neuron.
     The synapse density fields are further used as probability distributions to Poisson sample 
-    mutiple realizations of synaptic connections between pre-synaptic cells, and the post-synaptic cell.
+    mutiple realizations of synaptic connections between pre-synaptic cells, and the post-synaptic cell
+    (see :py:meth:`~singlecell_input_mapper.singlecell_input_mapper.synapse_mapper.SynapseMapper.create_synapses`).
     
     Attributes:
         cells (dict): 
