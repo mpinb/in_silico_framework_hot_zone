c29d1e50647e5b82050789ad92a368084d7b504e
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-09-23 10:24:38 +0200

    add debug filter

--- docs/conf.py
+++ docs/conf.py
@@ -69,13 +69,11 @@ def debug_filter(value, item_type):
     logger.debug(f"Found {item_type}: {value}")
     return value
 
-# Function to add the custom filter to the Jinja2 environment
 def setup(app):
-    app.connect('builder-inited', add_jinja2_filters)
+    app.connect('env-before-read-docs', add_jinja2_filters)
 
-def add_jinja2_filters(app):
-    app.builder.templates.environment.filters['debug'] = debug_filter
-
+def add_jinja2_filters(app, env, docnames):
+    env.filters['debug'] = debug_filter
 
 # Napoleon settings
 napoleon_google_docstring = True
