81425685d7cdfc908d6084a8c551a7e0c62f684d
bgmeulem <>
2024-09-25 13:34:50 +0200

    document cell modify funs

--- single_cell_parser/cell_modify_functions/__init__.py
+++ single_cell_parser/cell_modify_functions/__init__.py
@@ -1,34 +1,49 @@
-'''
-Created on Feb 16, 2019
+"""Modify the cell after it is initalized.
 
-@author: abast
+Such a function can for example be used to scale the apical dendrite diameter.
 
-This package is contains  functions, that modify the cell after
-it is initalized.
+If a cell has been modified this way, the cell parameter file contains the key ``cell_modify_functions``
+in its neuron section. This is a nested dictionary, where the keys are the names of the cell modification
+functions, and the values are the keyword arguments as a dictionary.
 
-Such a function can for example be used to scale the apical dendrite.
+See also:
+    The :ref:`cell_parameters_format` file format.
 
-These kind of modifying functions have caused a lot of trouble in the past, as it 
-has not been specified in the neuron parameterfile. No record of whether a modification
-had been applied was saved alongside the simulation data.
+Example:
+    >>> cell_parameters.neuron.cell_modify_functions
+    {'scale_apical': {'scale': 1.5}}
+    >>> from single_cell_parser.cell_modify_functions import get
+    >>> fun = get('scale_apical')
+    >>> fun
+    <function scale_apical at 0x7f0c3f2b6e18>
+    >>> print(fun.__doc__)
+    Scale the apical dendrite of a cell.
 
-Now, the cell parameter file can contain the key "cell_modify_functions" in its
-neuron section. The value corresponding to that key is a dictionary. The keys of that 
-dictionary can be names to modules in this package. The values corresponding to these keys
-are the keyword arguments of the respective cell modify function.
+    Args:
+        cell (:class:`~single_cell_parser.cell.Cell`): The cell to scale.
+        scale (float): The scaling factor.
+        compartment (str): The compartment to scale.
+            If "ApicalDendrite", the cell is assumed to have sections with label "ApicalDendrite".
+            If "Trunk", the cell is assumed to have ``detailed_labels`` assigned manually, or by :py:meth:`biophysics_fitting.utils.augment_cell_with_detailed_labels`.
+            Currently, only "ApicalDendrite" and "Trunk" are supported compartments.
+    
+    Returns:
+        :class:`~single_cell_parser.cell.Cell`: The scaled cell.
 
-I.e.
+    Raises:
+        ValueError: If the compartment is not "ApicalDendrite" or "Trunk".
 
-{'neuron': {
-    'Soma': { ... }
-    'Dendrite': { ... }
-    'filename': {some hocpath}
-    'cell_modify_functions': {
-        'scale_apical': {'scale': 2.1}
-    }}}
-'''
+"""
+
+# These kind of modifying functions have caused a lot of trouble in the past, as it 
+# had not been specified in the neuron parameterfile. No record of whether a modification
+# had been applied was saved alongside the simulation data.
+# Now, it is in the neruon parameter file under the key "cell_modify_functions".
 
 import importlib
+
+__author__ = "Arco Bast"
+__date__ = "2019-02-16"
 
 
 def get(funname):
--- single_cell_parser/cell_modify_functions/apical_current_injection.py
+++ single_cell_parser/cell_modify_functions/apical_current_injection.py
@@ -1,15 +1,35 @@
+"""Inject a current at a given distance from the soma."""
+
 from biophysics_fitting.setup_stim import setup_soma_step
 
 
-def apical_current_injection(cell,
-                             amplitude=None,
-                             delay=None,
-                             duration=None,
-                             dist=None):
+def apical_current_injection(
+        cell,
+        amplitude=None,
+        delay=None,
+        duration=None,
+        dist=None):
+    """Inject a current at a given distance from the soma.
+    
+    Args:
+        cell (:class:`~single_cell_parser.cell.Cell`): The cell object.
+        amplitude (float): The amplitude of the current (nA).
+        delay (float): The delay of the current (ms).
+        duration (float): The duration of the current (ms).
+        dist (float): The distance from the soma (um).
+            For an apical current injection, this should be the distance from the soma to the apical dendrite.
+    
+    Returns:
+        :class:`~single_cell_parser.cell.Cell`: The cell with the current injection set up.
+
+    See also:
+        :py:meth:`biophysics_fitting.setup_stim.setup_soma_step`
+    """
     # note: setup_soma_step has been extended to support a dist parameter
-    setup_soma_step(cell,
-                    amplitude=amplitude,
-                    delay=delay,
-                    duration=duration,
-                    dist=dist)
+    setup_soma_step(
+        cell,
+        amplitude=amplitude,
+        delay=delay,
+        duration=duration,
+        dist=dist)
     return cell
