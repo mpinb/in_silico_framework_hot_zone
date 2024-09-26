01daf84d167329cbab3cdb8397b7b4a0717928f5
bgmeulem <>
2024-09-26 11:36:29 +0200

    doc vm analysis

--- single_cell_parser/analyze/membrane_potential_analysis.py
+++ single_cell_parser/analyze/membrane_potential_analysis.py
@@ -65,13 +65,14 @@ def compute_mean_psp_amplitude(vTraces, tStim, dt, width=35.0, t_delay=15.0):
     """Compute the mean amplitude of all PSPs across multiple voltage traces.
     
     The post-synaptic potential (PSP) amplitude is the maximum membrane voltage deflection
-    between :paramref:`tStim` + ``delay`` and :paramref:`tStim` + ``delay`` + ``width``,
-    where ``delay`` and ``width`` are :math:`15.0` and :math:`35.0 \, ms` respectively.
+    between :paramref:`tStim` + :paramref:`t_delay` and :paramref:`tStim` + :paramref:`delay` + :paramref:`width`.
 
     Args:
-        vTraces (list): List of voltage traces
-        tStim (float): Time of stimulation
-        dt (float): Time step
+        vTraces (list): List of voltage traces.
+        tStim (float): Timepoint of stimulation (in ms).
+        dt (float): Time step of the voltage traces.
+        width (float): Width of the PSP window. Default is 35.0 ms.
+        t_delay (float): Delay of the PSP window (i.e. time between the stimulus and the PSP onset). Default is 15.0 ms.
 
     Returns:
         tuple: A tuple containing the delay and the mean amplitude of the PSPs.    
@@ -89,13 +90,13 @@ def compute_vm_std_windows(vStd, tStim, dt, width=35.0, window_start_times=None)
     if window_start_times is None:
         window_start_times = [-50.0, 15.0]
     """Compute the standard deviation of the voltage during different time windows.
-    
-    Time windows are defined as :math:`[-50.0, 15.0] \, ms` and :math:`[15.0, 50.0] \, ms`.
-    
+        
     Args:
         vStd (list): List of standard deviations of the voltage
         tStim (float): Time of stimulation
-        dt (float): Time step
+        dt (float): Time step.
+        width (float): Width of each time window. Default is 35.0 ms.
+        window_start_times (list): List of start times for the time windows. Default is [-50.0, 15.0].
         
     Returns:
         tuple: A tuple containing the time windows and the average standard deviations.
@@ -109,6 +110,15 @@ def compute_vm_std_windows(vStd, tStim, dt, width=35.0, window_start_times=None)
 
 
 def compute_vm_histogram(vTraces, bins=None):
+    """Compute the histogram of membrane voltage traces.
+    
+    Args:
+        vTraces (numpy.ndarray): Array of (one or more) voltage traces. Shape: (``n_traces``, ``n_timepoints``).
+        bins (numpy.ndarray): Bins for the histogram. Default is ``np.arange(-80, -40, 2)``.
+
+    Returns:
+        tuple: The histogram and the bins
+    """
     if bins is None:
         bins = np.array([-80.0 + i * 2.0 for i in range(21)])
     vPoints = vTraces.flatten()
@@ -119,12 +129,24 @@ def compute_vm_histogram(vTraces, bins=None):
 
 
 def compute_uPSP_amplitude(t, v, tSyn, isEPSP=True, t_width_baseline=10.0):
-    '''
-    simple method for determining amplitude of
-    unitary PSP starting at time tSyn:
-    Determine baseline as median Vm during 10ms
-    preceding the PSP
-    In case of IPSPs set isEPSP=False
+    '''Compute the amplitude of a uPSP.
+    
+    Simple method for determining amplitude of a unitary post-synaptic potential (uPSP) 
+    
+    Args:
+        t (list): Time vector
+        v (list): Membrane potential vector
+        tSyn (float): Timepoint of synaptic activation.
+        isEPSP (bool): 
+            If True, the uPSP is an excitatory post-synaptic potential (EPSP).
+            If False, the uPSP is an inhibitory post-synaptic potential (IPSP). 
+            Default is True.
+        t_width_baseline (float): 
+            Width of the time window to calculate the baseline membrane voltage. 
+            Default is 10.0 ms.
+    
+    Returns:
+        float: Amplitude of the uPSP.
     '''
     if len(t) != len(v):
         errstr = 'Time vector and membrane potential vector do not match'
@@ -157,13 +179,24 @@ def simple_spike_detection(
         tEnd=None,
         threshold=0.0,
         mode='regular'):
-    '''
-    Simple spike detection method. Identify
-    spike times within optional window [tBegin, tEnd]
-    by determining threshold crossing times from below
-    supported modes:
-    regular: absolute threshold crossing
-    differential: threshold crossing of dv/dt
+    '''Detect spike times in a voltage trace.
+
+    Simple spike detection method. Identifies spike times within optional window ``[tBegin, tEnd]``
+    by determining :paramref:`threshold` crossing times from below.
+
+    Args:
+        t (array): Time vector
+        v (array): Membrane potential vector.
+        tBegin (float, optional): Start time of the detection window. Default is None (begin of voltage trace).
+        tEnd (float, optional): End time of the detection window. Default is None (end of voltage trace).
+        threshold (float, optional): Threshold for spike detection (mV). Default is :math:`0.0 mV`.
+        mode (str, optional):
+            Mode for spike detection. Default is ``regular``.
+            - ``regular``: Checks if the membrane potential crosses an absolute :paramref:`threshold`.
+            - ``slope``: Checks if :math:`dV/dt` is larger than a :paramref:`threshold`.
+
+    Returns:
+        list: List of spike times.
     '''
     if len(t) != len(v):
         errstr = 'Dimensions of time vector and membrane potential vector not matching'
@@ -188,11 +221,16 @@ def simple_spike_detection(
             if v[i - 1] < threshold and v[i] >= threshold:
                 tSpike.append(t[i])
 
-    if mode == 'slope':
+    elif mode == 'slope':
         dvdt = np.diff(v) / np.diff(t)
         for i in range(beginIndex, endIndex):
             if dvdt[i - 1] < threshold and dvdt[i] >= threshold:
                 tSpike.append(t[i])
+
+    else:
+        errstr = 'Unknown mode for spike detection: %s' % mode
+        errstr += '\nSupported modes: regular, slope'
+        raise NotImplementedError(errstr)
 
     return tSpike
 
@@ -203,11 +241,17 @@ def PSTH_from_spike_times(
         tBegin=None,
         tEnd=None,
         aligned=True):
-    '''
-    Calculate PSTH from vectors of spike times
-    with optional bin size and time window [tBegin, tEnd].
-    If aligned=True, then the bins are aligned to
-    integer multiples of the bin size.
+    '''Calculates a PSTH from spike times.
+
+    Args:
+        spikeTimeVectors (list): List of spike time vectors.
+        binSize (float, optional): Bin size for the PSTH. Default is 1.0 ms.
+        tBegin (float, optional): Start time of the PSTH. Default is None (min of :paramref:`spikeTimeVectors`).
+        tEnd (float, optional): End time of the PSTH. Default is None (max of :paramref:`spikeTimeVectors`).
+        aligned (bool, optional): If True, aligns the bins to integer multiples of the bin size. Default is True.
+
+    Returns:
+        tuple: Tuple containing the histogram and the bins.
     '''
     norm = len(spikeTimeVectors)
     allSpikeTimes = []
@@ -240,9 +284,15 @@ def PSTH_from_spike_times(
 
 
 class RecordingSiteManager(object):
-    '''
-    simple class providing methods to handle recording
-    sites defined by Amira LandmarkSet
+    '''Parse AMIRA recording sites from a ``.landmarkAscii`` file.
+
+    Args:
+        landmarkFilename (str): Path to the landmark file.
+        cell (:class:`single_cell_parser.cell.Cell`): Cell object associated with the landmarks.
+
+    Attributes:
+        recordingSites (list): List of recording sites.
+        cell (:class:`single_cell_parser.cell.Cell`): Cell object.
     '''
     recordingSites = None
     cell = None
@@ -260,10 +310,17 @@ class RecordingSiteManager(object):
             self.recordingSites.append(newRecSite)
 
     def set_up_recording_site(self, location, ID, filename):
-        '''
-        determine section/segment on cell corresponding
-        to recording site location and create new
-        RecordingSite
+        '''Set up a :class:`RecordingSite` from a location.
+        
+        Determines the section and segment on the cell corresponding
+        to the recording site location and creates new :class:`RecordingSite`.
+
+        Used during initialization.
+
+        Args:
+            location (numpy.ndarray): Location of the recording site.
+            ID (int): ID of the recording site.
+            filename (str): Path to the AMIRA landmark file containing all recording sites.
         '''
         # inaccurate version
         #        minDist = 1e9
@@ -293,6 +350,8 @@ class RecordingSiteManager(object):
         minSegID = None
         minx = None
         minSegx = None
+        
+        # Find point closest to the recording location
         for i in range(len(self.cell.sections)):
             sec = self.cell.sections[i]
             for j in range(len(sec.pts)):
@@ -303,6 +362,7 @@ class RecordingSiteManager(object):
                     minDist = dist
                     minSecID = i
                     minx = ptx
+        # Find segment closest to the point
         mindx = 1.0e9
         for i in range(len(self.cell.sections[minSecID].segx)):
             x = self.cell.sections[minSecID].segx[i]
@@ -316,15 +376,16 @@ class RecordingSiteManager(object):
         somaDist = self.cell.distance_to_soma(sec, minSegx)
         splitName = filename.split('/')[-1]
         tmpIndex = splitName.find('.landmarkAscii')
-        label = splitName[:
-                          tmpIndex] + '_ID_%03d_sec_%03d_seg_%03d_x_%.3f_somaDist_%.1f' % (
-                              ID, minSecID, minSegID, minSegx, somaDist)
+        # Create descriptive label
+        label = \
+            splitName[:tmpIndex] + \
+            '_ID_%03d_sec_%03d_seg_%03d_x_%.3f_somaDist_%.1f' % (ID, minSecID, minSegID, minSegx, somaDist)
+        # Set up recsite
         newRecSite = RecordingSite(minSecID, minSegID, label)
         return newRecSite
 
     def update_recordings(self):
-        '''
-        copy recorded voltages at recording sites
+        '''Add the :class:`~single_cell_parser.cell.Cell`'s recorded voltages to the :paramref:`recordingSites`.
         '''
         for recordingSite in self.recordingSites:
             secID = recordingSite.secID
@@ -333,8 +394,13 @@ class RecordingSiteManager(object):
             recordingSite.vRecordings.append(vTrace)
 
     def _read_landmark_file(self, landmarkFilename):
-        '''
-        returns list of (x,y,z) points
+        '''Read the AMIRA landmark file and return the landmarks.
+
+        Args:
+            landmarkFilename (str): Path to the AMIRA landmark file.
+
+        Returns:
+            list: List of landmarks coordinates, where each element is of format (x,y,z).
         '''
         if not landmarkFilename.endswith('.landmarkAscii'):
             errstr = 'Wrong input format: has to be landmarkAscii format. Path: {p}'.format(
@@ -362,11 +428,23 @@ class RecordingSiteManager(object):
 
 
 class RecordingSite(object):
-    '''
-    light-weight container holding information
-    about recording site:
-    section and segment ID of attached cell,
-    identifier label, and list of recorded voltage vectors
+    '''Dataclass for a recording site.
+
+    Attributes:
+        secID (int): Section ID of the recording site.
+        segID (int): Segment ID of the recording site.
+        label (str): Identifier label.
+        vRecordings (list): List of recorded voltage vectors.
+            Parsed from the :class:`~single_cell_parser.cell.Cell`.
+
+    Args:
+        secID (int): Section ID of the recording site.
+        segID (int): Segment ID of the recording site.
+        label (str): Identifier label.
+
+    See also:
+        The :class:`~single_cell_parser.analyze.membrane_potential_analysis.RecordingSiteManager`
+        class for setting up recording sites and parsing their voltage traces.
     '''
     secID = None
     segID = None
@@ -381,10 +459,12 @@ class RecordingSite(object):
 
 
 class SpikeInit:
-    '''
-    Provides methods for analysis of spike initiation
-    in in vivo/ in vitro simulations. Can be used to
-    obtain features of spike shape, adaptation etc...
+    '''Analyze spike initiation.
+
+    Can be used to obtain features of spike shape, adaptation etc...
+
+    See also:
+        :class:`biophysics_fitting.evaluator.Evaluator` for a more exhaustive analysis of voltage traces.
     '''
 
     def __init__(self):
@@ -393,12 +473,19 @@ class SpikeInit:
 
     @staticmethod
     def vm_steady_state(cell, tVec, tStim, dtStim):
-        '''
-        computes "steady-state" subthreshold voltage from
-        voltage trace during stimulation time window
-        [tStim, tStim+dtStim]
-        this can be used to define voltage spike threshold
-        at current intensities just below AP initiation
+        '''Computes the "steady-state" voltage 
+        
+        The steady-state voltage is the median voltage during a stimulation time window.
+        This can be used to define a voltage spike threshold at current intensities just below AP initiation.
+
+        Args:
+            cell (:class:`~single_cell_parser.cell.Cell`): Cell object.
+            tVec (list): Time vector.
+            tStim (float): Start time of stimulation.
+            dtStim (float): Duration of stimulation.
+
+        Returns:
+            float: Steady-state voltage during :math:`[tStim, tStim+dtStim]`.
         '''
         t = np.array(tVec)
         v = np.array(cell.soma.recVList[0])
@@ -418,9 +505,18 @@ class SpikeInit:
 
     @staticmethod
     def analyze_single_spike(cell, tVec, thresh):
-        '''
-        for comparison with in vitro spike shape:
-        compute spike height, FWHM (duration), AHP
+        '''Calculate spike height, width (FWHM) and after hyperpolarization depth (AHP).
+
+        Only does this for a single spike in the voltage trace: the one with the
+        maximum deflection from the threshold.
+
+        Args:
+            cell (:class:`~single_cell_parser.cell.Cell`): Cell object.
+            tVec (list): Time vector.
+            thresh (float): Spike threshold.
+
+        Returns:
+            tuple: Tuple containing the spike height, full width half max (FWHM), and after hyperpolarization (AHP).
         '''
         t = np.array(tVec)
         v = np.array(cell.soma.recVList[0])
@@ -442,4 +538,8 @@ class SpikeInit:
 
     @staticmethod
     def analyze_spike_train():
+        """Analyze a spike train.
+        
+        :skip-doc:
+        """
         pass
