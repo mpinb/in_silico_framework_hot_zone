e8f42025b37616ec07ade72df3eadde896275ae4
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-10-11 17:13:21 +0200

    bugfix

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,6 @@ import os
 project_root = os.path.join(os.path.abspath(os.pardir))
 sys.path.insert(0, project_root)
 from docs.parse_notebooks import copy_and_parse_notebooks_to_docs
-from docs.nodoc import skip_member, modules_to_skip
 from docutils.parsers.rst import Directive
 from sphinx.util.docutils import SphinxDirective
 from jinja2 import Template
@@ -62,6 +61,87 @@ autoapi_options = [
 rst_prolog = """
 .. role:: summarylabel
 """
+
+"""Configure modules, functions, methods, classes and attributes so that they are not documented by Sphinx."""
+import ast, os
+from unittest.mock import patch
+
+project_root = os.path.join(os.path.abspath(os.pardir))
+
+def skip_member(app, what, name, obj, skip, options):
+    """Skip members if they have the :skip-doc: tag in their docstring."""
+    # Debug print to check what is being processed
+    # print(f"Processing {what}: {name}")
+    
+    # skip special members, except __get__ and __set__
+    if name.startswith('__') and name.endswith('__') and name not in ['__get__', '__set__']:
+        return True
+    
+    # Skip if it has the :skip-doc: tag
+    if obj.__doc__ and ':skip-doc:' in obj.__doc__:
+        if ':skip-doc:' in obj.__doc__:
+            # print(f"Docstring for {name}: {obj.__doc__}")
+            print(f"Skipping {what}: {name} due to :skip-doc: tag")
+            return True
+    
+    # Skip inherited members
+    if hasattr(obj, '__objclass__') and obj.__objclass__ is not obj.__class__:
+        return True
+    
+    modules_to_skip = find_modules_with_tag(project_root, tag=":skip-doc:")
+    if name in modules_to_skip:
+        print(f"Skipping {what}: {name} due to :skip-doc: tag in module {obj.__module__}")
+        return True
+    
+    return skip
+    
+def get_module_docstring(module_path):
+    """Get the docstring of a module without importing it."""
+    try:
+        # Find the module's file path
+        print("Module path:", module_path)
+        if not os.path.isfile(module_path):
+            raise FileNotFoundError(f"Module file {module_path} not found")
+
+        # Read the module's source code
+        with open(module_path, 'r', encoding='utf-8') as f:
+            source_code = f.read()
+
+        # Parse the source code
+        parsed_ast = ast.parse(source_code)
+
+        # Extract the docstring
+        docstring = ast.get_docstring(parsed_ast)
+        return docstring
+
+    except Exception as e:
+        print(f"Error getting docstring for module {module_path}: {e}")
+        return None
+
+def find_modules_with_tag(source_dir, tag=":skip-doc:"):
+    """Recursively find all modules with a specific tag in their docstring.
+    
+    Returns:
+        List of module path glob patterns with the tag.
+    """
+    modules_with_tag = []
+
+    for root, dirs, files in os.walk(source_dir):
+        for f in files:
+            if f.endswith(".py"):
+                module_path = os.path.join(root, f)
+                docstring = get_module_docstring(module_path)
+                if docstring and tag in docstring:
+                    if "__init__" in module_path:
+                        modules_with_tag.append(module_path.rstrip('__init__.py') + "**")
+                    else:
+                        modules_with_tag.append(module_path + "**")                
+
+    return modules_with_tag
+
+
+# Use the cached result
+modules_to_skip = ['**tests**', '**barrel_cortex**', '**installer**', '**__pycache__**'] + find_modules_with_tag(project_root, tag=":skip-doc:")
 
 # skipping documentation for certain members
 print("ignoring modules: ", modules_to_skip)
