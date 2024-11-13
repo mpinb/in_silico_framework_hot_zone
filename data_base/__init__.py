2706c39eb5f91978eb53f4c4de5b73b55d1b44bd
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-11-13 17:56:32 +0100

    typo

--- data_base/__init__.py
+++ data_base/__init__.py
@@ -3,6 +3,7 @@ Efficient, reproducable and flexible database whose API mimics a python dictiona
 This package provides efficient and scalable methods to store and access simulation results at a terrabyte scale.
 Each data base entry contains metadata, indicating when the data was written, and the exact version of the source code that was used at this timepoint.
 A wide variety of input data and output file formats are supported (see :py:mod:`data_base.IO.LoaderDumper`), including:
+
 - 1D and ND numpy arrays
 - pandas and dask dataframes
 - :py:class:`~single_cell_parser.cell.Cell` and :py:class:`simrun.reduced_model.get_kernel.ReducedLdaModel`
@@ -12,10 +13,12 @@ Simulation results from :py:mod:`single_cell_parser` and :py:mod:`simrun` can be
 This package's ``IO`` and ``db_initializers`` subpackages can be adapted, changing the data base system. 
 Which data base system to use is decided by :py:mod:`~data_base.data_base`.
 This way:
+
 1. Future support (or deprecation) for file formats can be easily implemented by creating a new ``IO.LoaderDumper`` submodule.
 2. Backwards compatibility to old data or old formats is guaranteed, as the previous database system is still available.
 
 Available data base systems:
+
 1. :py:mod:`~data_base.isf_data_base`
 2. :py:mod:`~data_base.model_data_base` (deprecated)
 """
