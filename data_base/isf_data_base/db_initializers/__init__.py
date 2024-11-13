c113616fd798a2ad75cb8f423ced073d7cb62ea1
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-11-13 13:08:26 +0100

    add db initializers init docs

--- data_base/isf_data_base/db_initializers/__init__.py
+++ data_base/isf_data_base/db_initializers/__init__.py
@@ -1,4 +1,10 @@
 """Initialize a database from raw simulation data.
 
-This package provides methods for initializing databases from the results of simulations for various purposes.
+This package provides modules for initializing databases from simulation results.
+
+:py:mod:`~data_base.isf_data_base.db_initializers.load_simrun_general` provides a general
+way to parse raw simulation output to intermediate pickle files, or permanent dask and pandas dataframes.
+A database that has been initialized with this module is herafter called a "simrun-initialized" database.
+
+Each other submodule provides an ``init`` method, which builds on top of the previously simrun-initialized data.
 """
