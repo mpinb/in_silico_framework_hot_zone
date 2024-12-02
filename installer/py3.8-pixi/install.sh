01bcbebec766a324c8132e9cd9552801767fa31d
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2024-12-02 15:47:39 +0100

    clean up installers pixi does not require an installer folder rename legacy back to jsut py3.8 for consistency leave miniforge for now

--- installer/py3.8-pixi/install.sh
+++
@@ -1,7 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-shell=$SHELL
-curl -fsSL https://pixi.sh/install.sh | bash
-echo 'eval "$(pixi completion --shell {$SHELL})"' >> ~/.{$SHELL}rc
-source ~/.bashrc
-shell $SCRIPT_DIR/install_pandas_msgpack.sh
-pixi install
