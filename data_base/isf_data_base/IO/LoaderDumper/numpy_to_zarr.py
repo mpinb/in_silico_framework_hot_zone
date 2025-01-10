8b1b0ac61914f834dac1e20947e62f1097679f53
bgmeulem <bjorge1997@hotmail.com>
2025-01-10 13:51:58 +0100

    add numpy to zarr dumper for ISFDB add small numpy to zarr test

---
+++ data_base/isf_data_base/IO/LoaderDumper/numpy_to_zarr.py
@@ -0,0 +1,38 @@
+"""Read and write a numpy array to the ``zarr`` format.
+
+See also:
+    https://zarr.readthedocs.io/en/stable/api/zarr/storage/index.html#zarr.storage.LocalStore
+"""
+
+
+import os, json
+# import cloudpickle
+import numpy as np
+from . import parent_classes
+import zarr
+
+
+def check(obj):
+    """Check whether the object can be saved with this dumper
+    
+    Args:
+        obj (object): Object to be saved
+        
+    Returns:
+        bool: Whether the object is a numpy object.
+    """
+    return isinstance(obj, np)
+
+
+class Loader(parent_classes.Loader):
+    """Loader for zarr objects"""
+    def get(self, savedir):
+        return zarr.load(os.path.join(savedir, 'obj.zarr'))
+
+
+def dump(obj, savedir):
+    """Write out an object in .zarr format."""
+    zarr.save_array(os.path.join(savedir, 'obj.zarr'), obj)
+
+    with open(os.path.join(savedir, 'Loader.json'), 'w') as f:
+        json.dump({'Loader': __name__}, f)
