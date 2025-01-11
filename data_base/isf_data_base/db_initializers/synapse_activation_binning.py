e05c13c185d2663116338762eba82dc60dc58e70
bgmeulem <bjorge1997@hotmail.com>
2025-01-11 14:09:42 +0100

    remove msgpack imports for Py3

--- data_base/isf_data_base/db_initializers/synapse_activation_binning.py
+++ data_base/isf_data_base/db_initializers/synapse_activation_binning.py
@@ -19,7 +19,7 @@ from functools import partial
 import numpy as np
 import dask, six
 from data_base.analyze.temporal_binning import universal as temporal_binning
-from data_base.isf_data_base.IO.LoaderDumper import numpy_to_zarr, numpy_to_msgpack
+from data_base.isf_data_base.IO.LoaderDumper import numpy_to_zarr
 import logging
 logger = logging.getLogger("ISF").getChild(__name__)
 try:
@@ -28,6 +28,7 @@ except ImportError:
     logger.warning("Could not import excitatory/inhibitory celltypes from barrel_cortex. Is the module available?")
 
 if six.PY2:
+    from data_base.isf_data_base.IO.LoaderDumper import numpy_to_msgpack
     numpy_dumper = numpy_to_msgpack
 elif six.PY3:
     numpy_dumper = numpy_to_zarr
