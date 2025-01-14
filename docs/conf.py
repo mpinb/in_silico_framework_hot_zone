a105db80a271c4469d9cdb5d49fb901de32499c6
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-01-14 17:42:11 +0100

    test books theme

--- docs/conf.py
+++ docs/conf.py
@@ -343,7 +343,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "furo"  # sphinx_immaterial is nice, but requires more config
+html_theme = "sphinx_book_theme" 
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
