19e597f1975a58f33af8105319542a82c9a3b7b4
bgmeulem <bjorge1997@hotmail.com>
2025-02-04 23:29:57 +0100

    [minor] add some docs

--- biophysics_fitting/MOEA_EH_minimal/__init__.py
+++ biophysics_fitting/MOEA_EH_minimal/__init__.py
@@ -1,3 +1,10 @@
 """
-This package provides mechanisms and NEURON configuration files to run BluePyOpt :cite:`Van_Geit_Gevaert_Chindemi_Roessert_Courcol_Muller_Schuermann_Segev_Markram_2016`.
+This package provides mechanisms and NEURON configuration files to 
+run Hay's evolutionary algorithm :cite:t:`Hay_Hill_Schuermann_Markram_Segev_2011` for the optimization of
+biophysical models.
+
+For the sake of self-containment, this directory provides copies of all mechanisms required for cortical L5 neurons.
+However, these are not the mechanisms used in ISF. ISF compiles and loads mechanisms from the ``mechanisms`` directory.
+If you ahve segfault issues, make sure those are compiled and loaded into the neuron hoc namespace. You can
+do this by simply importing ``mechanisms.l5pt``.
 """
