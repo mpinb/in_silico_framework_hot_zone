145d4ff4f55dcd2c9207840d729347bfe8dc7391
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-02-24 17:37:48 +0100

    [minor] bugfix double name

--- data_base/isf_data_base/isf_data_base.py
+++ data_base/isf_data_base/isf_data_base.py
@@ -8,7 +8,7 @@ import os, tempfile, string, json, threading, random, shutil, inspect, datetime,
 from pathlib import Path
 from data_base import _module_versions, data_base_register
 import data_base.exceptions as db_exceptions
-from data_base.utils import colorize
+from data_base.utils import colorize_str
 VC = _module_versions.version_cached
 
 logger = logging.getLogger("ISF").getChild(__name__)
@@ -1010,7 +1010,7 @@ class ISFDataBase:
         """
         return self._get_str()  # print with default depth and max_lines
 
-    def ls(self, depth=0, max_depth=2, max_lines=20, all_files=False, max_lines_per_key=3, colorize=True):
+    def ls(self, depth=0, max_depth=2, max_lines=20, all_files=False, max_lines_per_key=3, color=True):
         """Prints out the content of the database in a tree structure.
         
         In addition to simply printing it out, this method allows to specify how the tree should look.
@@ -1026,9 +1026,9 @@ class ISFDataBase:
         """
         print(self._get_str(
             depth=depth, max_depth=max_depth, max_lines=max_lines, 
-            all_files=all_files, max_lines_per_key=max_lines_per_key, colorize=colorize))
+            all_files=all_files, max_lines_per_key=max_lines_per_key, color=color))
     
-    def _get_str(self, depth=0, max_depth=2, max_lines=20, all_files=False, max_lines_per_key=3, colorize=True):
+    def _get_str(self, depth=0, max_depth=2, max_lines=20, all_files=False, max_lines_per_key=3, color=True):
         """Fetches a string representation for this db in a tree structure.
         
         This is internal API and should never be called directly.
@@ -1047,8 +1047,8 @@ class ISFDataBase:
         str_.append("Located at {}".format(self._basedir))
         # str_.append("{1}DataBases{0} | {2}Directories{0} | {3}Keys{0}".format(
         #     bcolors.ENDC, bcolors.OKGREEN, bcolors.WARNING, bcolors.OKCYAN) )
-        if colorize:
-            str_.append(colorize(self._basedir.name, bcolors.OKGREEN))
+        if color:
+            str_.append(colorize_str(self._basedir.name, bcolors.OKGREEN))
         else:
             str_.append(self._basedir.name)
         lines = calc_recursive_filetree(
@@ -1057,7 +1057,7 @@ class ISFDataBase:
             max_lines_per_key=max_lines_per_key, 
             all_files=all_files, 
             max_lines=max_lines,
-            colorize=colorize)
+            colorize=color)
         for line in lines:
             str_.append(line)
         return "\n".join(str_)
