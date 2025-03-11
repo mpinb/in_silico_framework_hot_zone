25933b4e04d633b2172094abdc5a24db215d872c
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-03-11 14:31:42 +0100

    [bugifx] test mdb

--- tests/test_data_base/model_data_base/__init__.py
+++ tests/test_data_base/model_data_base/__init__.py
@@ -1,15 +1,23 @@
 from __future__ import absolute_import
-import os, sys, shutil, tempfile
-import distributed
-import pytest
+
+import os
+import sys
 
 parent = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.insert(0, parent)
 import distributed
-from data_base import utils
+
 import getting_started
 from mechanisms import l5pt as l5pt_mechanisms
+
 # set up paths
-test_data_folder = os.path.join(getting_started.parent, \
-                              'example_simulation_data', \
-                              'C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/')
+test_data_folder = os.path.join(
+    getting_started.parent,
+    "example_simulation_data",
+    "C2_evoked_UpState_INH_PW_1.0_SuW_0.5_C2center/",
+)
+
+# To test model-data_base in the way it existed before, we must register it as a top-level module
+import data_base.model_data_base
+
+sys.modules["model_data_base"] = data_base.model_data_base
