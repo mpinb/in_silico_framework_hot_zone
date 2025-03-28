60a8a243162313ff358549ec9b9560f0faa76fd0
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-03-28 11:01:17 +0100

    Move default file locking config to config. File-based as default (no redis)

---
+++ config/file_locking.py
@@ -0,0 +1,6 @@
+DEFAULT_CONFIG = [
+    dict(type="file"),
+    # dict(type="redis", config=dict(host="spock", port=8885, socket_timeout=1)),
+    # dict(type="redis", config=dict(host="localhost", port=6379, socket_timeout=1)),
+]
+
