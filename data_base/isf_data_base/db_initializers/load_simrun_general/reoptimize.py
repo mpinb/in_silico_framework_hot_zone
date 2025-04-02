04178b0ad1e7abbec8214b01a658bca48ed1039f
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-04-02 17:28:48 +0200

    Add documentation

--- data_base/isf_data_base/db_initializers/load_simrun_general/reoptimize.py
+++ data_base/isf_data_base/db_initializers/load_simrun_general/reoptimize.py
@@ -13,16 +13,30 @@ import shutil, os
 import dask.dataframe as dd
 import logging
 from tqdm import tqdm
+from .config import DUMPERS_TO_REOPTIMIZE
 
 isf_logger = logging.getLogger("ISF")
 logger = isf_logger.getChild(__name__)
 
-DUMPERS_TO_REOPTIMIZE = [
-    "pandas_to_parquet",
-    "dask_to_parquet"
-]
-
 def _check_needs_reoptimization(key, old_dumper_name, new_dumper_name):
+    """Check if a key needs re-optimization.
+    
+    Dumper formats that need to be re-optimized are saved in :py:mod:`~data_base.isf_data_base.db_initializers.load_simrun_general.config`.
+    
+    Args:
+        key (str): The key to check.
+        old_dumper_name (str): The name of the old dumper.
+        new_dumper_name (str): The name of the new dumper.
+        
+    Returns:
+        bool: True if the key needs re-optimization, False otherwise.
+    
+    Raises:
+        DataBaseException: If the key is not in the database.
+        
+    Raises:
+        Warning: If the old and new dumper are configured to be the same (should not be the case).
+    """
     if new_dumper_name == old_dumper_name:
         logger.warning("I am configured to re-optimize the dumper `{}`, but the current default optimized dumper for `{}` is also `{}`. Skipping this key...".format(
             old_dumper_name, key, new_dumper_name))
@@ -31,10 +45,13 @@ def _check_needs_reoptimization(key, old_dumper_name, new_dumper_name):
         logger.debug("Reoptimizing `{}` from `{}` to `{}`".format(key, old_dumper_name, new_dumper_name))
         return True
 
+
 def _get_dumper_kwargs(d, client=None):
-    """
-    Get the dumper kwargs for a given DataFrame.
+    """Get the dumper kwargs for a given DataFrame.
+    
     This is used to determine if saving the dataframe requires a client or not.
+    
+    :skip-doc:
     """
     if isinstance(d, dd.DataFrame):
         assert client is not None, "Please provide a dask client to re-optimize the database."
@@ -63,9 +80,28 @@ def _reoptimize_key(db, key, new_dumper, client=None):
 
 
 def reoptimize_db(db, client=None, progress=False, n_db_parents=0, suppress_warnings=False):
-    """
-    Re-optimize a sub-database with a new dumper.
+    """Re-optimize a database with a new dumper.
+    
+    This function will re-optimize all keys in the database that are configured to be re-optimized.
     This is useful for switching between different data formats, such as from `parquet` to `msgpack`.
+    
+    It recurses into subdatabases, and re-optimizes them as well.
+    
+    Args:
+        db (:py:mod:`~data_base.data_base.isf_data_base.ISFDataBase`): 
+            The database to re-optimize.
+        client (dask.distributed.Client):
+            The dask client to use for re-optimizing the database.
+        progress (bool):
+            If True, show a progress bar for the re-optimization.
+            Subdatabases have nested progress bars.
+            It is recommended to also suppress warnings when using progress bars, so
+            the output stays readable.
+        suppress_warnings (bool):
+            If True, suppress warnings during re-optimization.
+            
+    Returns:
+        None
     """
     assert client is not None, "Please provide a dask client to re-optimize the database."
     logger.info("Reoptimizing database at {}".format(db.basedir))
