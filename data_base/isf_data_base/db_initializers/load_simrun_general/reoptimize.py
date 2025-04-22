b8f787eedeb024a2c520b9d4cc3dfc1f077a55c7
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-04-22 16:03:23 +0200

    [bugfix] maybe the backend needs a `copy()`?

--- data_base/isf_data_base/db_initializers/load_simrun_general/reoptimize.py
+++ data_base/isf_data_base/db_initializers/load_simrun_general/reoptimize.py
@@ -68,13 +68,17 @@ def _reoptimize_key(db, key, new_dumper, client=None):
     shutil.move(path_to_key, temp_path)  # move original key to tmp location
     
     if hasattr(db, "_sql_backend"):  # mdb compat
-        db._sql_backend[temp_key] = db._sql_backend[key]
+        db._sql_backend[temp_key] = db._sql_backend[key].copy()
         db._sql_backend[temp_key].relpath = temp_key
     
     try:
+        logger.debug("Reoptimizing `{}` to `{}`".format(key, temp_key))
+        logger.debug("Temproarily moving `{}` to `{}`".format(key, temp_key))
         d = db[temp_key]
         kwargs = _get_dumper_kwargs(d, client=client)
+        logger.debug("Saving `{}` with new dumper `{}`".format(key, new_dumper.__name__))
         db.set(key, d, dumper=new_dumper, **kwargs)
+        logger.debug("Removing temporary key `{}`".format(temp_key))
         shutil.rmtree(temp_path)
     except Exception as e:
         if os.path.exists(path_to_key):
