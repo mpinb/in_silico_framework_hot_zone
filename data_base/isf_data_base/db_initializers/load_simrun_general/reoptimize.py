eb41837e33c2f579b33d50f64d3cf84c149a2e2c
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-04-22 15:37:57 +0200

    idem

--- data_base/isf_data_base/db_initializers/load_simrun_general/reoptimize.py
+++ data_base/isf_data_base/db_initializers/load_simrun_general/reoptimize.py
@@ -75,14 +75,14 @@ def _reoptimize_key(db, key, new_dumper, client=None):
         kwargs = _get_dumper_kwargs(d, client=client)
         db.set(key, d, dumper=new_dumper, **kwargs)
         shutil.rmtree(temp_path)
-        if hasattr(db, "_sql_backend"):  # mdb compat
-            del db._sql_backend[temp_key]
     except Exception as e:
         if os.path.exists(path_to_key):
             shutil.rmtree(path_to_key)  
         shutil.move(temp_path, path_to_key)
-        del db._sql_backend[temp_key]
         raise DataBaseException(f"Failed to re-optimize {key}") from e
+    finally:
+        if hasattr(db, "_sql_backend"):  # mdb compat
+            del db._sql_backend[temp_key]
 
 
 def reoptimize_db(db, client=None, progress=False, n_db_parents=0, suppress_warnings=False):
