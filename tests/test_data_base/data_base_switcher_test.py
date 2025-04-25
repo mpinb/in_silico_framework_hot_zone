9c3e9ee08c2a879d3e813a0f90b56f16c6f469ff
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-04-25 15:07:02 +0200

    fix db backend switcher test

--- tests/test_data_base/data_base_switcher_test.py
+++ tests/test_data_base/data_base_switcher_test.py
@@ -2,7 +2,14 @@
 import os, sys, importlib
 import config, data_base
 
-def test_switch_db_backend(tmp_path):
+def _set_isf_use_mdb(val="0"):
+    os.environ["ISF_USE_MDB"] = val
+    # clean context
+    _reload_modules()
+    # Remove python-cached modules to ensure they are reloaded
+    del data_base.IO
+
+def test_switch_db_backend(tmpdir):
     """
     Test if you can switch between ISF Data Base and Model Data Base
     using the ISF_USE_MDB environment variable.
@@ -10,26 +17,22 @@ def test_switch_db_backend(tmp_path):
 
     # Save the original value of the environment variable
     original_env_value = os.environ.get("ISF_USE_MDB")
+    tmp_db_path = os.path.join(tmpdir, "test_db")
+    tmp_mdb_path = os.path.join(tmpdir, "test_mdb")
 
     try:
-        os.environ["ISF_USE_MDB"] = "0"
-        _reload_modules()
+        _set_isf_use_mdb("0")  # Set to "0" (use ISF Data Base)
         assert config.isf_is_using_mdb() == False, "Expected ISF_USE_MDB to be False"
-        db = data_base.DataBase(tmp_path)
+        db = data_base.DataBase(tmp_db_path)
         assert isinstance(db, data_base.isf_data_base.ISFDataBase), "Database is not of type ISFDataBase"
         from data_base import IO
         assert IO.__name__.startswith("data_base.isf_data_base.IO"), \
             f"Wrong IO package imported. Expected 'data_base.isf_data_base.IO', but got '{IO.__name__}'"
 
-        # Clean up the context
-        db.remove()
-        _reload_modules()
-        del data_base.IO
-
         # Test with ISF_USE_MDB set to "1" (use Model Data Base)
-        os.environ["ISF_USE_MDB"] = "1"
+        _set_isf_use_mdb("1")
         assert config.isf_is_using_mdb() == True, "Expected ISF_USE_MDB to be True"
-        db = data_base.DataBase(tmp_path)
+        db = data_base.DataBase(tmp_mdb_path)
         assert isinstance(db, data_base.model_data_base.ModelDataBase), "Database is not of type ModelDataBase"
         from data_base import IO
         assert IO.__name__.startswith("data_base.model_data_base.IO"), \
