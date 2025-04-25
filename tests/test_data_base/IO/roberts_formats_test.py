f3f6a8845f114ab26d2f6499054c5b9b687cfa23
Bjorge Meulemeester <bjorge.meulemeester@mpinb.mpg.de>
2025-04-25 14:55:51 +0200

    sim triail test fix

--- tests/test_data_base/IO/roberts_formats_test.py
+++ tests/test_data_base/IO/roberts_formats_test.py
@@ -1,8 +1,11 @@
 from data_base.IO.roberts_formats import read_pandas_synapse_activation_from_roberts_format, write_pandas_synapse_activation_to_roberts_format
 from tests.test_data_base import test_data_folder
 from pandas.util.testing import assert_frame_equal
-import os
-
+import os, inspect
+if "sim_trail_index" in inspect.signature(write_pandas_synapse_activation_to_roberts_format).parameters:
+    sim_triail = "sim_trail_index"
+else:
+    sim_triail = "sim_trial_index"
 
 def test_saved_and_reloaded_synapse_file_is_identical(tmpdir):
     synapse_file_path = os.path.join(
@@ -11,16 +14,18 @@ def test_saved_and_reloaded_synapse_file_is_identical(tmpdir):
         'simulation_run0000_synapses.csv')
     assert os.path.exists(synapse_file_path)
     synapse_pdf = read_pandas_synapse_activation_from_roberts_format(\
-                            synapse_file_path, sim_trial_index = 'asdasd')
+                            synapse_file_path, **{sim_triail: 'asdasd'})
 
     try:
         path_file = os.path.join(tmpdir.dirname, 'test.csv')
         write_pandas_synapse_activation_to_roberts_format(
             path_file, synapse_pdf)
         synapse_pdf_reloaded = read_pandas_synapse_activation_from_roberts_format(
-            path_file, sim_trial_index='asdasd')
+            path_file, 
+            **{sim_triail: 'asdasd'})
     except:
         raise
 
-    assert_frame_equal(synapse_pdf.dropna(axis=1, how='all'),
-                       synapse_pdf_reloaded.dropna(axis=1, how='all'))
+    assert_frame_equal(
+        synapse_pdf.dropna(axis=1, how='all'),
+        synapse_pdf_reloaded.dropna(axis=1, how='all'))
